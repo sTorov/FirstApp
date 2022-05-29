f3c89ca83371dc66aeac2742e5f0c2d6b209ea12
sTorov <sotorov2011@yandex.ru>
2022-05-30 00:18:50 +0300

    Вывод на экран цифр в различных системах счисления. Вывод вещественных цифр

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,8 +4,10 @@ class FirstApp
 {
 	static void Main(string[] args)
 	{
-        Console.WriteLine(true);
-		Console.WriteLine(false);
+		Console.WriteLine(5);
+		Console.WriteLine(5.5);
+		Console.WriteLine(0x0a);
+        Console.WriteLine(0b11);
 		
 		Console.ReadKey();
 	}
