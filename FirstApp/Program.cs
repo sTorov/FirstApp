5ac2aea2a121d9a29489d731d04597935de109c9
sTorov <sotorov2011@yandex.ru>
2022-05-30 00:14:03 +0300

    Вывод логических литералов true false

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,17 +4,9 @@ class FirstApp
 {
 	static void Main(string[] args)
 	{
-
-		const string myName = "Dima";
-
-		Console.WriteLine(myName);
-
-		Console.WriteLine("\tПривет, мир");
-		Console.WriteLine("\tМне 26 лет");
-		Console.WriteLine("\tMy name is\n Dima");
-        Console.WriteLine('\u0040');
-        Console.WriteLine('\x23');
-
+        Console.WriteLine(true);
+		Console.WriteLine(false);
+		
 		Console.ReadKey();
 	}
 }
