b6810e8ffb3f81473d4755acdb25254f5f7c1439
sTorov <sotorov2011@yandex.ru>
2022-05-29 23:01:51 +0300

    Добавьте файлы проекта.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class FirstApp
+{
+	public static void Main(string[] args)
+	{
+
+		string myName;
+		myName = "Jane";
+
+		Console.WriteLine(myName);
+
+		Console.ReadKey();
+	}
+}
