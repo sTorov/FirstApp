9817f1faa2fd6422a4cf1004d0d84de8b5d1ffff
sTorov <sotorov2011@yandex.ru>
2022-06-02 02:15:48 +0300

    Явное преобразование типов
    Преобразование переменной типа int в переменную типа byte.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,17 +4,9 @@ class FirstApp
 {
 	static void Main(string[] args)
 	{
+		int olddata = 6;
+		byte data = (byte) olddata;
 
-		string myName = "Dima";
-		byte myAge = 26;
-		bool myPet = false;
-		double myLeg = 40.5;
-
-		Console.WriteLine($"My name is {myName}");
-		Console.WriteLine($"My age is {myAge}");
-		Console.WriteLine($"Do I have a pet? {myPet}");
-        Console.WriteLine($"My shoe size is {myLeg}");
-        
 		Console.ReadKey();
 	}
 }
