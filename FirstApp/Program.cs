ba69e924aa64cea2498bcf4023d6038729d33fc5
sTorov <sotorov2011@yandex.ru>
2022-06-02 01:18:57 +0300

    Постфиксный инкремент

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,7 +5,8 @@ class FirstApp
 	static void Main(string[] args)
 	{
 		int counter = 10;
-		Console.WriteLine("Value: {0} Pref_Increment: {1}", counter, ++counter);
+		Console.WriteLine("Value: {0} Increment: {1}\n", counter, counter++);
+        Console.WriteLine($"Post_Increment: {counter}");
 
 		Console.ReadKey();
 	}
