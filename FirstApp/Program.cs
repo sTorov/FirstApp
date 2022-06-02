b8266d24cdabb473f67c4afcb259532fbff93b62
sTorov <sotorov2011@yandex.ru>
2022-06-02 03:37:38 +0300

    Задание 3.7.4 дополнено
    Переменные age и day - изменён тип данных на var
    Добавлены переменные grouth и myshoe типа var
    Добавлен вывод на экран роста и размера обуви

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -15,16 +15,23 @@ class FirstApp
     
     static void Main(string[] args)
     {
+        var grouth = 181;
+        var myshoe = 41.5;
+
         Console.Write("Enter your name: ");
         string name = Console.ReadLine();
 
         Console.Write("Enter your age: ");
-        byte age = checked((byte)int.Parse(Console.ReadLine()));
+        var age = checked( (byte) int.Parse(Console.ReadLine()));
+
+        Console.WriteLine();
 
         Console.WriteLine($"Your name is {name} and age is {age}");
+        Console.WriteLine($"What is my grouth? {grouth} sm");
+        Console.WriteLine($"What is my shoe size? {myshoe}\n");
 
         Console.Write("What is your favorite days of week? ");
-        DaysOfWeek day = (DaysOfWeek)byte.Parse(Console.ReadLine());
+        var day = (DaysOfWeek) byte.Parse(Console.ReadLine());
         Console.WriteLine($"Your favorite day is {day}");
 
         Console.ReadKey();
