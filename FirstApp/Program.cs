d92b85e83965e2a776cef2509d709b64ba4ee8c2
sTorov <sotorov2011@yandex.ru>
2022-06-02 03:13:19 +0300

    Задание 3.7.4 Любимый день недели

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,13 +2,30 @@
 
 class FirstApp
 {
-	static void Main(string[] args)
+	enum DaysOfWeek
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    
+    static void Main(string[] args)
     {
         Console.Write("Enter your name: ");
         string name = Console.ReadLine();
+
         Console.Write("Enter your age: ");
-        byte age = checked ((byte) int.Parse(Console.ReadLine()));
+        byte age = checked((byte)int.Parse(Console.ReadLine()));
+
         Console.WriteLine($"Your name is {name} and age is {age}");
+
+        Console.Write("What is your favorite days of week? ");
+        DaysOfWeek day = (DaysOfWeek)byte.Parse(Console.ReadLine());
+        Console.WriteLine($"Your favorite day is {day}");
 
         Console.ReadKey();
 	}
