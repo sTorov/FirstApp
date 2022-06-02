32fb3b658bf045d9de18ea6f0f23ede3b8a5d060
sTorov <sotorov2011@yandex.ru>
2022-06-02 03:10:34 +0300

    Служебное слово checked

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,7 +7,7 @@ class FirstApp
         Console.Write("Enter your name: ");
         string name = Console.ReadLine();
         Console.Write("Enter your age: ");
-        byte age = (byte) int.Parse(Console.ReadLine());
+        byte age = checked ((byte) int.Parse(Console.ReadLine()));
         Console.WriteLine($"Your name is {name} and age is {age}");
 
         Console.ReadKey();
