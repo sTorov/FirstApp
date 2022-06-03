33ab0e69ac9cbd4c46d01b963bfcf04d10ca17c5
sTorov <sotorov2011@yandex.ru>
2022-06-04 02:04:22 +0300

    Какой цвет вы любите? Цикл For

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,21 +1,45 @@
 ﻿using System;
 
-class FirstApp
+namespace Unit4
 {
-    static void Main(string[] args)
+    class Logic
     {
-        Console.Write("Enter your name: ");
-        var name = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какой ваш любимый цвет?");
+                        
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Itration " + i);
+                
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;         //Цвет фона текста в консоли
+                        Console.ForegroundColor = ConsoleColor.White;       //Цвет шрифта
+                        Console.WriteLine("Ваш любимый цвет красный!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш любимый цвет зеленый!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш любимый цвет бирюзовый!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ваш любимый цвет желтый!");
+                        break;
+                }
+            }
+            
+            
 
-        Console.Write("Enter your age: ");
-        var age = int.Parse(Console.ReadLine());
-
-        Console.WriteLine($"Your name is {name} and age is {age}");
-
-        Console.Write("Enter your birthdate: ");
-        var birthdate = Console.ReadLine();
-        Console.WriteLine($"Your birthdate is {birthdate}");
-
-        Console.ReadKey();
-	}
+            Console.ReadLine();
+        }
+    }
 }
