5f50c0482ff891aaa1b996440242a69d81b7d4a6
sTorov <sotorov2011@yandex.ru>
2022-06-04 02:04:22 +0300

    Задание 4.1.18 Какой ваш любимый цвет? Switch

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,23 +10,28 @@ namespace Unit4
 
             var color = Console.ReadLine();
 
-            if (color == "red")
+            switch (color)
             {
-                Console.BackgroundColor = ConsoleColor.Red;         //Цвет фона текста в консоли
-                Console.ForegroundColor = ConsoleColor.Black;       //Цвет шрифта
-                Console.WriteLine("Ваш любимый цвет красный!");
-            }
-            else if (color == "green")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Ваш любимый цвет зеленый!");
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Ваш любимый цвет бирюзовый!");
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;         //Цвет фона текста в консоли
+                    Console.ForegroundColor = ConsoleColor.White;       //Цвет шрифта
+                    Console.WriteLine("Ваш любимый цвет красный!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Ваш любимый цвет зеленый!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Ваш любимый цвет бирюзовый!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ваш любимый цвет желтый!");
+                    break;
             }
 
             Console.ReadLine();
