2b0f993727bf5b6ceba0309f296270c512e50978
sTorov <sotorov2011@yandex.ru>
2022-06-05 22:32:48 +0300

    Бесконечный цикл While, с возможностью его остановки

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,75 +5,49 @@ namespace Unit4
     class Logic
     {
         static void Main(string[] args)
-        {
-
-            Console.WriteLine("\nWHILE\n");
-            
+        {            
             int k = 0;
-            while (k < 0)
-            {
+
+            while (true)
+            {                
                 Console.WriteLine("Какой ваш любимый цвет?");
                 Console.WriteLine(k);
-                switch (Console.ReadLine())
+
+                var text = Console.ReadLine();
+
+                if (text == "stop")
                 {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;         //Цвет фона текста в консоли
-                        Console.ForegroundColor = ConsoleColor.White;       //Цвет шрифта
-                        Console.WriteLine("Ваш любимый цвет красный!");
-                        break;
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("Ваш любимый цвет зеленый!");
-                        break;
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("Ваш любимый цвет бирюзовый!");
-                        break;
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Ваш любимый цвет желтый!");
-                        break;
+                    Console.WriteLine("Цикл остановлен");
+                    break;
                 }
+                
+                switch (text)
+                    {
+                        case "red":
+                            Console.BackgroundColor = ConsoleColor.Red;         //Цвет фона текста в консоли
+                            Console.ForegroundColor = ConsoleColor.White;       //Цвет шрифта
+                            Console.WriteLine("Ваш любимый цвет красный!");
+                            break;
+                        case "green":
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.WriteLine("Ваш любимый цвет зеленый!");
+                            break;
+                        case "cyan":
+                            Console.BackgroundColor = ConsoleColor.Cyan;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.WriteLine("Ваш любимый цвет бирюзовый!");
+                            break;
+                        default:
+                            Console.BackgroundColor = ConsoleColor.Yellow;
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Ваш любимый цвет желтый!");
+                            break;
+                    }
+                
                 k++;
             }
-
-            Console.WriteLine("\nDO WHILE\n");
-
-            int t = 0;
-            do
-            {
-                Console.WriteLine("Какой ваш любимый цвет?");
-                Console.WriteLine(t);
-                switch (Console.ReadLine())
-                {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;         //Цвет фона текста в консоли
-                        Console.ForegroundColor = ConsoleColor.White;       //Цвет шрифта
-                        Console.WriteLine("Ваш любимый цвет красный!");
-                        break;
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("Ваш любимый цвет зеленый!");
-                        break;
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("Ваш любимый цвет бирюзовый!");
-                        break;
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Ваш любимый цвет желтый!");
-                        break;
-                }
-
-                t++;
-            } while (t < 0);
-
+            
             Console.ReadLine();
         }
     }
