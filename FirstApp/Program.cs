3edda0b6b5c2bebe62bd758e37ab80b07032738e
sTorov <sotorov2011@yandex.ru>
2022-06-07 19:12:10 +0300

    Ваш любимый цвет. Массивы
    Вывод проипомощи цикла Foreach

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,51 +5,42 @@ namespace Unit4
     class Logic
     {
         static void Main(string[] args)
-        {            
-            int k = 0;
+        {
+            string[] favcolor = new string[3];
 
-            while (true)
-            {                
-                Console.WriteLine("Какой ваш любимый цвет?");
-                Console.WriteLine(k);
-
-                var text = Console.ReadLine();
-
-                if (text == "stop")
-                {
-                    Console.WriteLine("Цикл остановлен");
-                    break;
-                }
-                
-                switch (text)
-                    {
-                        case "red":
-                            Console.BackgroundColor = ConsoleColor.Red;         //Цвет фона текста в консоли
-                            Console.ForegroundColor = ConsoleColor.White;       //Цвет шрифта
-                            Console.WriteLine("Ваш любимый цвет красный!");
-                            break;
-                        case "green":
-                            Console.BackgroundColor = ConsoleColor.Green;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            Console.WriteLine("Ваш любимый цвет зеленый!");
-                            break;
-                        case "cyan":
-                            Console.BackgroundColor = ConsoleColor.Cyan;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            Console.WriteLine("Ваш любимый цвет бирюзовый!");
-                            break;
-                        case "yellow":
-                            Console.BackgroundColor = ConsoleColor.Yellow;
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Ваш любимый цвет желтый!");
-                            break;
-                        default:
-                            continue;
-                    }
-                
-                k++;
+            for (int i = 0; i < favcolor.Length; i++)
+            {
+                Console.WriteLine($"Введите ваш любимый цвет {i + 1}: ");
+                favcolor[i] = Console.ReadLine();
             }
-            
+
+            foreach (var color in favcolor)
+            {
+                switch (color)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Ваш любимый цвет красный!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш любимый цвет зеленый!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш любимый цвет бирюзовый!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ваш любимый цвет желтый!");
+                        break;
+                }
+            }
+         
             Console.ReadLine();
         }
     }
