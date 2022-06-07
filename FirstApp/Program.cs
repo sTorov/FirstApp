6f8df5704736dc25d82a8dab71fb5eca051bb825
sTorov <sotorov2011@yandex.ru>
2022-06-07 22:10:56 +0300

    Задание 4.3.12
    Сортировка массива по возрастанию

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,42 +6,64 @@ namespace Unit4
     {
         static void Main(string[] args)
         {
-            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
-
-            for (int i = 0; i < array.GetLength(0); i++)
+            int[] arr = { 5, 6, 9, 9, 4, 1, 2, 3, 4, 9 };
+            int[] sort = new int [arr.Length];
+            
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i, j] + "\t");
-                }
-                Console.WriteLine();
+                Console.Write(arr[i] + " ");
             }
-
-            Console.WriteLine(array.GetLength(0));
-            Console.WriteLine(array.GetUpperBound(0));
-
-            //GetLength
-
-            for (int i = 0; i < array.GetLength(1); i++)
-            {
-                for (int j = 0; j < array.GetLength(0); j++)
-                {
-                    Console.Write(array[j, i] + "\t");
-                }
-                Console.WriteLine();
-            }
-
-            //GetUpperBound
 
             Console.WriteLine();
 
-            for (int i = 0; i <= array.GetUpperBound(1); i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j <= array.GetUpperBound(0); j++)
+                int index = 0;
+                int copy = 0;
+
+                for (int j = 0; j < arr.Length; j++)
                 {
-                    Console.Write(array[j, i] + "\t");
+                    if (arr[i] > arr[j])
+                        index++;
+                    if (arr[i] == arr[j])
+                        copy++;
                 }
-                Console.WriteLine();
+                if (copy == 1)
+                    sort[index] = arr[i];
+                else
+                    for (int k = index; k < index + copy; k++)
+                    {
+                        sort[k] = arr[i];
+                    }
+            }
+
+            for (int i = 0; i < sort.Length; i++)
+            {
+                Console.Write(sort[i] + " ");
+            }
+
+            //Правильный вариант
+
+            int temp;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
             }
 
             Console.ReadKey();
