473ebb816edbd67148b4d7d87783adeff3534396
sTorov <sotorov2011@yandex.ru>
2022-06-07 23:56:06 +0300

    Задание 4.4.2 Кортежи
    Запись данных пользователя в кортеж anketa

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,17 +4,19 @@ class FirstApp
 {
     static void Main(string[] args)
     {
+        (string name, string birthdate, int age) anketa;
+        
         Console.Write("Enter your name: ");
-        var name = Console.ReadLine();
+        anketa.name = Console.ReadLine();
 
         Console.Write("Enter your age: ");
-        var age = int.Parse(Console.ReadLine());
+        anketa.age = int.Parse(Console.ReadLine());
 
-        Console.WriteLine($"Your name is {name} and age is {age}");
+        Console.WriteLine($"Your name is {anketa.name} and age is {anketa.age}");
 
         Console.Write("Enter your birthdate: ");
-        var birthdate = Console.ReadLine();
-        Console.WriteLine($"Your birthdate is {birthdate}");
+        anketa.birthdate = Console.ReadLine();
+        Console.WriteLine($"Your birthdate is {anketa.birthdate}");
 
         Console.ReadKey();
 	}
