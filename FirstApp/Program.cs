78f13e7845b2d832692af3bdd741b642b70e5af3
sTorov <sotorov2011@yandex.ru>
2022-06-07 23:06:09 +0300

    Задание 4.3.16
    Количество положительных элементов в массиве
    Двумерный массив

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,22 +6,46 @@ namespace Unit4
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 2, 3, 4, -1, -5, 0, -6, 1, -4 };
+            int[,] array =
+            {
+                { 1, 2, 3, 4, -1, -5 },
+                { -12, 4, 0, 0, 3, -5}
+            };
+                
             int positive = 0;
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                Console.Write(array[i] + " ");
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
             }
             Console.WriteLine();
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                if (array[i] > 0)
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > 0)
+                        positive++;
+                }
+            }
+
+            Console.WriteLine("Количество положительных элементов: " + positive);
+            
+            // foreach
+            positive = 0;
+
+            foreach (var item in array)
+            {
+                if (item > 0)
                     positive++;
             }
 
             Console.WriteLine("Количество положительных элементов: " + positive);
+
             Console.ReadLine();
         }
     }
