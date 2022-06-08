2cecdad65c0c27b265de972673ae52e178b7e195
sTorov <sotorov2011@yandex.ru>
2022-06-08 15:10:05 +0300

    Задание 4.5.6
    Повторение опраса для 3-х пользователей. (цикл While)

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,31 +5,42 @@ class FirstApp
     static void Main(string[] args)
     {
         (string Name, string SurName, string Login, int LoginCount, bool Pet, double Age, string[] FavColor) User;
+        int k = 1;
 
-        Console.Write("Введите имя: ");
-        User.Name = Console.ReadLine();
-
-        Console.Write("Введите фамилия: ");
-        User.SurName = Console.ReadLine();
-
-        Console.Write("Введите логин: ");
-        User.Login = Console.ReadLine();
-        User.LoginCount = User.Login.Length;
-
-        Console.WriteLine("Есть ли у вас животное (да/нет) : ");
-        string PetAnswer = Console.ReadLine();
-        User.Pet = PetAnswer == "Да" || PetAnswer == "да" ? true : false;
-
-        Console.WriteLine("Введите возраст: ");
-        User.Age = double.Parse(Console.ReadLine());
-
-        User.FavColor = new string[3]; 
-        Console.WriteLine("Введите 3 любимых цвета: ");
-        for (int i = 0; i < User.FavColor.Length; i++)
+        while (k < 4)
         {
-            Console.Write($"{i + 1} Цвет: ");
-            User.FavColor[i] = Console.ReadLine();
+            Console.WriteLine("Пользователь номер " + k + "\n");
+            
+            Console.Write("Введите имя: ");
+            User.Name = Console.ReadLine();
+
+            Console.Write("Введите фамилия: ");
+            User.SurName = Console.ReadLine();
+
+            Console.Write("Введите логин: ");
+            User.Login = Console.ReadLine();
+            User.LoginCount = User.Login.Length;
+
+            Console.WriteLine("Есть ли у вас животное (да/нет) : ");
+            string PetAnswer = Console.ReadLine();
+            User.Pet = PetAnswer == "Да" || PetAnswer == "да" ? true : false;
+
+            Console.WriteLine("Введите возраст: ");
+            User.Age = double.Parse(Console.ReadLine());
+
+            User.FavColor = new string[3];
+            Console.WriteLine("Введите 3 любимых цвета: ");
+            for (int i = 0; i < User.FavColor.Length; i++)
+            {
+                Console.Write($"{i + 1} Цвет: ");
+                User.FavColor[i] = Console.ReadLine();
+            }
+
+            k++;
+            Console.WriteLine();
         }
-                
+
+        Console.ReadLine();
+
     }
 }
