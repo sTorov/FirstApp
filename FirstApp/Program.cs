0f6697e432be736d86fa7bd766a51d4beee1ebfc
sTorov <sotorov2011@yandex.ru>
2022-06-08 14:50:46 +0300

    Задание 4.5.5
    Написан код для ввода возраста пользователя и трех его любимых цветов. (цикл For)

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -19,5 +19,17 @@ class FirstApp
         Console.WriteLine("Есть ли у вас животное (да/нет) : ");
         string PetAnswer = Console.ReadLine();
         User.Pet = PetAnswer == "Да" || PetAnswer == "да" ? true : false;
+
+        Console.WriteLine("Введите возраст: ");
+        User.Age = double.Parse(Console.ReadLine());
+
+        User.FavColor = new string[3]; 
+        Console.WriteLine("Введите 3 любимых цвета: ");
+        for (int i = 0; i < User.FavColor.Length; i++)
+        {
+            Console.Write($"{i + 1} Цвет: ");
+            User.FavColor[i] = Console.ReadLine();
+        }
+                
     }
 }
