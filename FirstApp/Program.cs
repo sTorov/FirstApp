e98d8d909bbb81e6152dd787468a9e746e06e040
sTorov <sotorov2011@yandex.ru>
2022-06-08 14:20:07 +0300

    Задание 4.5.2
    Заполниние кортежа User значениями с клавиатуры для полей: имя пользователя, фамилия, логин.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,5 +6,13 @@ class FirstApp
     {
         (string Name, string SurName, string Login, int LoginCount, bool Pet, double Age, string[] FavColor) User;
 
-	}
+        Console.Write("Введите имя: ");
+        User.Name = Console.ReadLine();
+
+        Console.Write("Введите фамилия: ");
+        User.SurName = Console.ReadLine();
+
+        Console.Write("Введите логин: ");
+        User.Login = Console.ReadLine();
+    }
 }
