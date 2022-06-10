286c7b9dbc7ca51cecb4125929ecb1e499477b4b
sTorov <sotorov2011@yandex.ru>
2022-06-10 22:14:29 +0300

    Unit 5. Создание метода ShowColor

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,21 +2,58 @@
 
 class FirstApp
 {
-    static void Main(string[] args)
+    class Logic
     {
-        (string Name, string[] Dishes) User;
-
-        Console.Write("Введите имя: ");
-        User.Name = Console.ReadLine();
-
-        Console.WriteLine("Введите 5 любимых блюд");
-        User.Dishes = new string[5];
-        for (int i = 0; i < User.Dishes.Length; i++)
+        static void ShowColor()
         {
-            Console.Write($"Блюдо {i + 1}: ");
-            User.Dishes[i] = Console.ReadLine();
+            Console.WriteLine("Какой ваш любимый цвет? (eng)");
+            string color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Ваш любимый цвет красный!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Ваш любимый цвет зеленый!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Ваш любимый цвет бирюзовый!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ваш любимый цвет желтый!");
+                    break;
+            }
         }
 
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            var (name, age) = ("Dima", 26);
+
+            Console.WriteLine($"Ваше имя {name}");
+            Console.WriteLine($"Ваш возраст {age}");
+
+            Console.Write("Введите ваше имя: ");
+            name = Console.ReadLine();
+
+            Console.Write("Введите ваш возраст: ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Ваше имя {name}");
+            Console.WriteLine($"Ваш возраст {age}");
+
+            ShowColor();
+
+            Console.ResetColor();
+            Console.ReadLine();
+        }
     }
 }
