f2e50e440c9b5012eb6a6ef01bdf22bb1f50cdd5
sTorov <sotorov2011@yandex.ru>
2022-06-10 21:19:50 +0300

    Модуль 5 Задание 1. Введение.
    Создан кортеж User, содержащий имя пользователя Name и массив с текстовой информацией о его пяти любимых блюдах Dishes. Имя пользователя  заполнено через ввод в консоли, а информация о блюдах — в цикле через консоль.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,43 +4,19 @@ class FirstApp
 {
     static void Main(string[] args)
     {
-        (string Name, string SurName, string Login, int LoginCount, bool Pet, double Age, string[] FavColor) User;
-        int k = 1;
+        (string Name, string[] Dishes) User;
 
-        while (k < 4)
+        Console.Write("Введите имя: ");
+        User.Name = Console.ReadLine();
+
+        Console.WriteLine("Введите 5 любимых блюд");
+        User.Dishes = new string[5];
+        for (int i = 0; i < User.Dishes.Length; i++)
         {
-            Console.WriteLine("Пользователь номер " + k + "\n");
-            
-            Console.Write("Введите имя: ");
-            User.Name = Console.ReadLine();
-
-            Console.Write("Введите фамилия: ");
-            User.SurName = Console.ReadLine();
-
-            Console.Write("Введите логин: ");
-            User.Login = Console.ReadLine();
-            User.LoginCount = User.Login.Length;
-
-            Console.WriteLine("Есть ли у вас животное (да/нет) : ");
-            string PetAnswer = Console.ReadLine();
-            User.Pet = PetAnswer == "Да" || PetAnswer == "да" ? true : false;
-
-            Console.WriteLine("Введите возраст: ");
-            User.Age = double.Parse(Console.ReadLine());
-
-            User.FavColor = new string[3];
-            Console.WriteLine("Введите 3 любимых цвета: ");
-            for (int i = 0; i < User.FavColor.Length; i++)
-            {
-                Console.Write($"{i + 1} Цвет: ");
-                User.FavColor[i] = Console.ReadLine();
-            }
-
-            k++;
-            Console.WriteLine();
+            Console.Write($"Блюдо {i + 1}: ");
+            User.Dishes[i] = Console.ReadLine();
         }
 
         Console.ReadLine();
-
     }
 }
