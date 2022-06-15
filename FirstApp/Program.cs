06ad87b93162676af6bff548131f4ccf3d40bdbe
sTorov <sotorov2011@yandex.ru>
2022-06-15 22:54:54 +0300

    Задание 5.5.3 (Вариант без Remove)

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,23 +2,35 @@
 
 class FirstApp
 {   
-    static void Echo(string phrase, int deep)
+    static void Echo(char[] modif, int deep, int h = 0)
     {
-        Console.WriteLine(phrase);
+        Console.Write("...");
+        for (int i = h; i < modif.Length; i++)
+        {
+            Console.Write(modif[i]);
+        }
+        Console.WriteLine();
 
         if (deep > 1)
-            Echo(phrase, --deep);       //depp - 1
+            Echo(modif, --deep, h + 2);       //depp - 1
     }
 
     static void Main(string[] args)
     {
         Console.WriteLine("Введите фразу");
         string str = Console.ReadLine();
+        char[] modif = new char[str.Length];
+        int i = 0;
+        foreach (var item in str)
+        {
+            modif[i] = item;
+            i++;
+        }
 
         Console.WriteLine("Глубина эха");
         int deep = int.Parse(Console.ReadLine());
 
-        Echo(str, deep);
+        Echo(modif, deep);
 
         Console.ReadLine();
     }
