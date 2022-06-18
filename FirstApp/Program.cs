daa96d49506f33c9f44d091707a21bb6b8348b0d
sTorov <sotorov2011@yandex.ru>
2022-06-18 23:12:43 +0300

    Иницализатор для класса Human

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,18 +1,75 @@
 ﻿using System;
 
-class Pen
-{
-    public string Color;
-    public int Cost;
+class Human
+{    
+    //Поля класса
+    public string name;
+    public int age;
 
-    public Pen()
+    //Метод класса
+    public void Greetings()
     {
-        Color = "Чёрный";
-        Cost = 100;
+        Console.WriteLine($"Меня зовут {name}, мне {age}");
+    }    
+
+    //Конструктор
+    public Human()
+    {
+        age = 99;
+        name = "Неизвестно";
     }
-    public Pen(string penColor,int penCost)
+
+    //Конструктор 2
+    public Human(string n)
     {
-        Color = penColor;
-        Cost = penCost;
+        name = n;
+        age = 99;
+    }
+
+    //Конструктор 3
+    public Human(string n, int a)
+    {
+        age = a;
+        name = n;
+    }
+}
+
+struct Animal
+{
+    //Поля структуры
+    public string type;
+    public string name;
+    public int age;
+
+    //Метод структуры
+    public void Info()
+    {
+        Console.WriteLine($"Это {type} по кличке {name}, ему {age}");
+    }
+
+    //Конструктор. Для структуры обязательно должны быть объявлены все поля
+    public Animal(string t, string n, int a)
+    {
+        type = t;
+        name = n;
+        age = a;
+    }
+}
+
+class General
+{
+    static void Main(string[] args)
+    {
+        Human person = new();
+        person.Greetings();
+
+        //Инициализатор
+        person = new Human{name = "Dima", age = 26};
+        person.Greetings();
+
+        //Animal animal = new("cat", "Barsik", 4);
+        //animal.Info();
+
+        Console.ReadLine();
     }
 }
