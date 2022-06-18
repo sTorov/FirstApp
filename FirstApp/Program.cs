5df32182a5f0fe8a15ccc2478b7652ace919fa57
sTorov <sotorov2011@yandex.ru>
2022-06-18 23:48:29 +0300

    Задание 6.2.8
    Создайте класс Rectangle для представления прямоугольников удовлетворяющий следующим требованиям:
    
    Класс должен содержать целочисленные поля для сторон a и b.
    
    Класс должен содержать метод Square, возвращающий площадь прямоугольника (произведение сторон).
    
    Класс должен содержать 3 конструктора: с 2 параметрами, когда a != b, с 1 параметром, когда a == b, и конструктор без параметров по умолчанию, в котором стороны будут заполняться как a = 6, b = 4.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,79 +1,40 @@
 ﻿using System;
 
-class Human
-{    
-    //Поля класса
-    public string name;
-    public int age;
-
-    //Метод класса
-    public void Greetings()
-    {
-        Console.WriteLine($"Меня зовут {name}, мне {age}");
-    }    
-
-    //Конструктор
-    public Human()
-    {
-        age = 99;
-        name = "Неизвестно";
-    }
-
-    //Конструктор 2
-    public Human(string n)
-    {
-        name = n;
-        age = 99;
-    }
-
-    //Конструктор 3
-    public Human(string n, int a)
-    {
-        age = a;
-        name = n;
-    }
-}
-
-struct Animal
+class Rectangle
 {
-    //Поля структуры
-    public string type;
-    public string name;
-    public int age;
-
-    //Метод структуры
-    public void Info()
+    public int a;
+    public int b;
+    
+    public void Params()
     {
-        Console.WriteLine($"Это {type} по кличке {name}, ему {age}");
+        Console.WriteLine($"a = {a}, b = {b}");
+    }
+    public int Square()
+    {
+        return a * b;
     }
 
-    //Конструктор. Для структуры обязательно должны быть объявлены все поля
-    public Animal(string t, string n, int a)
+    public Rectangle(int x, int y)
     {
-        type = t;
-        name = n;
-        age = a;
+        a = x;
+        b = y;
+    }
+    public Rectangle(int x)
+    {
+        a = b = x;
+    }
+    public Rectangle()
+    {
+        a = 6;
+        b = 4;
     }
 }
-
 class General
 {
     static void Main(string[] args)
     {
-        //Human person = new();
-        //person.Greetings();
-
-        ////Инициализатор
-        //person = new Human{name = "Dima", age = 26};
-        //person.Greetings();
-
-        Animal animal = new("cat", "Barsik", 4);
-        animal.Info();
-
-        //Инициализатор. Для структур не обязательно в инициализаторе указывать все поля
-        animal = new Animal {type = "Dog", name = "Tuzik", age = 6};
-        animal.Info();
-
-        Console.ReadLine();
+        Rectangle rect = new Rectangle(5);
+        rect.Params();
+        Console.WriteLine(rect.Square());
     }
 }
