13030663745b727169a4c85c8be1ac18bd85414f
sTorov <sotorov2011@yandex.ru>
2022-06-18 22:26:50 +0300

    Создание конструкторов для класса Human

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,27 +1,7 @@
 ﻿using System;
 
 class Human
-{
-    public class ClassHuman
-    {
-        public string Gender;
-
-        public void GenderInfo()
-        {
-            Console.WriteLine(Gender);
-        }
-    }
-
-    public struct StructHuman
-    {
-        public string Gender;
-
-        public void GenderInfo()
-        {
-            Console.WriteLine(Gender);
-        }
-    }
-
+{    
     //Поля класса
     public string name;
     public int age;
@@ -31,6 +11,27 @@ class Human
     {
         Console.WriteLine($"Меня зовут {name}, мне {age}");
     }    
+
+    //Конструктор
+    public Human()
+    {
+        age = 99;
+        name = "Неизвестно";
+    }
+
+    //Конструктор 2
+    public Human(string n)
+    {
+        name = n;
+        age = 99;
+    }
+
+    //Конструктор 3
+    public Human(string n, int a)
+    {
+        age = a;
+        name = n;
+    }
 }
 
 struct Animal
@@ -52,22 +53,19 @@ class General
     static void Main(string[] args)
     {
         Human person = new();
-        person.age = 43;
-        person.name = "Dima";
         person.Greetings();
 
-        Animal animal;
-        animal.age = 4;
-        animal.name = "Barsik";
-        animal.type = "cat";
-        animal.Info();
+        person = new("Dima");
+        person.Greetings();
 
-        Human.StructHuman newStruct;
-        newStruct.Gender = "Man";
-        newStruct.GenderInfo();
-
-        Human.ClassHuman newClass = new();
-        newClass.Gender = "Woman";
-        newClass.GenderInfo();
+        person = new("Dima", 26);
+        person.Greetings();
+        
+        //Animal animal;
+        //animal.age = 4;
+        //animal.name = "Barsik";
+        //animal.type = "cat";
+        //animal.Info();
+                
     }
 }
