246b6c0cdeb5d006895f6662835045a271ba91fe
sTorov <sotorov2011@yandex.ru>
2022-06-22 06:03:49 +0300

    Класс User(). Свойства только для чтения и только для записи

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,85 +1,25 @@
 ﻿using System;
 
-namespace UserWork
+class User
 {
-    class User
-    {
-        private int age;
-        public int Age
-        {
-            get
-            {
-                return age;
-            }
-            set
-            {
-                if (value < 18)
-                    Console.WriteLine("Возраст должен быть не меньше 18");
-                else
-                    age = value;
-            }
-        }
+	private string login;
+	private DateTime lastSeen;
 
-        private string login;
-        public string Login
-        {
-            get
-            {
-                return login;
-            }
-            set
-            {
-                if (value.Length < 3)
-                    Console.WriteLine("Логин должен содержать не менее 3-х символов");
-                else
-                    login = value;
-            }           
-        }
-        
-        private string email;
-        public string Email
-        {
-            get 
-            { 
-                return email;
-            }
-            set
-            {
-                int check = 0;
-                foreach (var item in value)
-                {
-                    if(item == '@')
-                    {
-                        check++;
-                        break;
-                    }
-                }
+	// Read-only
+	public string Login
+	{
+		get
+		{
+			return login;
+		}
+	}
 
-                if (check != 1)
-                    Console.WriteLine("Email должен содержать символ '@'");
-                else
-                    email = value;
-            }
-        }
-
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            User user = new User();
-
-            //Setter
-            user.Age = 23;
-            user.Login = "Dima";
-            user.Email = "Dimagmail.com";
-
-            //Getter
-            Console.WriteLine(user.Age);
-            Console.WriteLine(user.Login);
-            Console.WriteLine(user.Email);
-
-            Console.ReadLine();
-        }
-    }
+	// Write-only
+	public DateTime LastSeen
+	{
+		set
+		{
+			lastSeen = value;
+		}
+	}
 }
