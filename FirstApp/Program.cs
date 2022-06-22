7bc4ea199d513f4495effa12e2938a5c8efcc6ff
sTorov <sotorov2011@yandex.ru>
2022-06-22 04:51:55 +0300

    Задание 6.6.1 Класс TrafficLight()

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,61 +1,15 @@
 ﻿using System;
 
-enum TurnDirection
+class TrafficLigth
 {
-	None = 0,
-	Left,
-	Right
-}
-
-class Car
-{
-	private double Fuel;
-	private int Mileage;
 	private string color;
-	private TurnDirection turn;
 
-	public Car()
-	{
-		Fuel = 50;
-		Mileage = 0;
-		color = "White";
-	}
-
-	private void Move()
-	{
-		// Move a kilometer
-		Mileage++;
-		Fuel -= 0.5;
-	}
-
-	private void Turn(TurnDirection direction)
-	{
-		turn = direction;
-	}
-
-	public void FillTheCar()
-	{
-		Fuel = 50;
-	}
-
-	public string GetColor()
-	{
-		return color;
-	}
-
-	public void ChangeColor(string newColor)
-	{
-		if (color != newColor)
-			color = newColor;
-	}
-
-	public bool IsTurningLeft()
-	{
-		return turn == TurnDirection.Left;
-	}
-
-	public bool IsTurningRight()
-	{
-		return turn == TurnDirection.Right;
-	}
+	private void ChangeColor(string newColor)
+    {
+        color = newColor;
+    }
+    public string GetColor()
+    {
+        return color;
+    }
 }
