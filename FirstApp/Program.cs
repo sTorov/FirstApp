474c5cedbb8916f0dbcbe2ed7a3108fe7f80d9f1
sTorov <sotorov2011@yandex.ru>
2022-06-25 00:07:09 +0300

    Ключевые слова this
    Присвоение полю класса значения. Значение получено из переменной, наименование которой совпадает с наименованием поля класса.
    
    Также this позволяет обращаться к другому конструктору данного класса для его вызова перед указанным конструктором. Для этого достаточно после объявления конструктора и до фигурных скобок указать двоеточие и вызвать нужный конструктор через this(), по необходимости передавая ему параметры.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 
-class Food { }
-class Fruit : Food { }
-class Vegetable : Food { }
-class Apple : Fruit { }
-class Banana : Fruit { }
-class Pear : Fruit { }
-class Potato : Vegetable { }
-class Carrot : Vegetable { }
+class Obj
+{
+    private string name;
+    private string description;
+    private string type;
+
+    public Obj(string type)
+    {
+        //Console.WriteLine("Вызван конструктор без параметров");
+        this.type = type;
+    }
+    public Obj(string name, string description) : this("Тип") // this()
+    {
+        this.name = name;
+        this.description = description;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Obj obj = new Obj("Объект", "Нет описания");
+
+        Console.ReadKey();
+    }
+}
