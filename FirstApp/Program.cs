5003d017f24244ffc4b485cef809807dca28bdb7
sTorov <sotorov2011@yandex.ru>
2022-06-25 22:28:47 +0300

    Сокрытие. Ключевое слово new
    Классы A() B() C()
    + примеры и вызовы методов

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,26 +1,39 @@
-﻿using System;
-
-class BaseClass
+﻿class A
 {
     public virtual void Display()
     {
-        Console.WriteLine("Метод класса BaseClass");
+        Console.WriteLine("Вызван метод класса A");
     }
 }
-class DerivedClass : BaseClass
+class B : A
 {
     public override void Display()
     {
-        base.Display();
-        Console.WriteLine("Метод класса DerivedClass");
+        Console.WriteLine("Вызван метод класса B");
     }
-
 }
+class C : B
+{
+    public new void Display()
+    {
+        Console.WriteLine("Вызван метод класса C");
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        DerivedClass obj = new DerivedClass();
-        obj.Display();
+        A a = new A();
+        B b = new B();
+        C c = new C();
+
+        a.Display();        // метод класса A
+        b.Display();        // метод класса B
+        ((A)b).Display();   // метод класса B
+
+        c.Display();        // метод класса C
+        ((A)c).Display();   // метод класса B
+        ((B)c).Display();	// метод класса B
     }
 }
