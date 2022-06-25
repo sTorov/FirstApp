1d19725b86b60602264553f8b01a40042ad44ff4
sTorov <sotorov2011@yandex.ru>
2022-06-26 00:45:49 +0300

    Индексаторы. Ключевое слово this
    Рассмотрим работу индексаторов на примере класса BookCollection, который представляет коллекцию книг
    
    Индексаторы int и string

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,18 +1,77 @@
-﻿class Obj
+﻿// Класс для объекта “Книга”
+class Book
 {
-    public int Value;
-    public static Obj operator + (Obj a, Obj b)
+    public string Name;
+    public string Author;
+}
+// Класс для объекта “Коллекция книг”
+class BookCollection
+{
+    // Закрытое поле, хранящее книги в виде массива
+    private Book[] collection;
+
+    // Конструктор с добавлением массива книг
+    public BookCollection(Book[] collection)
     {
-        return new Obj
-        {
-            Value = a.Value + b.Value
-        };
+        this.collection = collection;
     }
-    public static Obj operator - (Obj a, Obj b)
+
+    // Индексатор по массиву
+    public Book this[int index]
     {
-        return new Obj
+        get
         {
-            Value = a.Value - b.Value
+            // Проверяем, чтобы индекс был в диапазоне для массива
+            if (index >= 0 && index < collection.Length)
+                return collection[index];
+            else
+                return null;
+        }
+        private set
+        {
+            // Проверяем, чтобы индекс был в диапазоне для массива
+            if (index >= 0 && index < collection.Length)
+                collection[index] = value;
+        }
+    }
+    public Book this[string name]
+    {
+        get
+        {
+            for (int i = 0; i < collection.Length; i++)
+            {
+                if (collection[i].Name == name)
+                    return collection[i];
+            }
+            return null;
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book[] array = new Book[]
+        {
+            new Book
+            {
+                Name = "Мастер и Маргарита",
+                Author = "М.А. Булгаков"
+            },
+            new Book
+            {
+                Name = "Отцы и дети",
+                Author = "И.С. Тургенев"
+            }
         };
+
+        BookCollection bookCollection = new BookCollection(array);
+
+        Book book = bookCollection[0];
+        Book book1 = bookCollection[-1];
+
+        Book book2 = bookCollection["Отцы и дети"];
+
+        Console.ReadKey();
     }
 }
