52a9aef0cb997932690e2bfaae9b4879d1878015
sTorov <sotorov2011@yandex.ru>
2022-06-25 21:43:00 +0300

    Переопределение свойств
    Классы Citizen()=>CivilServanr=>Presedent

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,17 +1,45 @@
-﻿using System;
-
-class BaseClass
+﻿class Citizen
 {
-    public virtual void Display()
+    public virtual double Age { get; set; }     //Виртуальное свойство
+
+    public void SayYourAge()
     {
-        Console.WriteLine("Метод класса BaseClass");
+        Console.WriteLine("Мне {0}", Age);
     }
 }
-class DerivedClass : BaseClass
+class CivilServant : Citizen
 {
-    public override void Display()
-    {
-        Console.WriteLine("Метод класса DerivedClass");
+    private double age;
+    public override double Age                  //Переопределённое свойство
+    { 
+        get
+        {
+            return age;
+        }
+        set 
+        { 
+            if(value < 18)
+                Console.WriteLine("Для работы госсслужащим гражданин должен быть не младше 18 лет");
+            else
+                age = value;
+        }
     }
-
+}
+class Presedent : Citizen                       
+{
+    private double age;
+    public override double Age                     //Переопределённое свойство
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            if (value < 35)
+                Console.WriteLine("Для работы презедентом гражданин должен быть не младше 35 лет");
+            else
+                age = value;
+        }
+    }
 }
