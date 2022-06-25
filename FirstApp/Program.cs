334e8d232bd9f1de948aae4e2ad020bda711aed4
sTorov <sotorov2011@yandex.ru>
2022-06-26 00:00:34 +0300

    Задание 7.2.12
    Для класса Obj перегрузите операторы + и -, чтобы результатом работы оператора был новый экземпляр класса Obj, а операции производились над полем Value.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,67 +1,18 @@
-﻿using System;
-class Vector
+﻿class Obj
 {
-    public int Y;
-    public int X;
-
-    public Vector Add(Vector second)
+    public int Value;
+    public static Obj operator + (Obj a, Obj b)
     {
-        return new Vector 
-        { 
-            X = this.X + second.X, 
-            Y = this.Y + second.Y 
-        };
-    }
-
-    //Реализация перегрузки оператора "+"
-    //1
-    public static Vector operator + (Vector A, Vector B)
-    {
-        return new Vector
+        return new Obj
         {
-            X = A.X + B.X,
-            Y = A.Y + B.Y
+            Value = a.Value + b.Value
         };
     }
-    //2
-    public static Vector operator + (Vector A)
+    public static Obj operator - (Obj a, Obj b)
     {
-        return new Vector
+        return new Obj
         {
-            X = A.X,
-            Y = A.Y
+            Value = a.Value - b.Value
         };
-    }
-    //3
-    public static Vector operator + (Vector A, (int X, int Y) B)
-    {
-        return new Vector
-        {
-            X = A.X + B.X,
-            Y = A.Y + B.Y
-        };
-    }
-
-}
-class Program
-{
-    static void Main(string[] args)
-    {
-        Vector A = new Vector { X = 3, Y = 5 };
-        Vector B = new Vector { X = 7, Y = 13 };
-
-        Vector C = new Vector { X = A.X + B.X, Y = A.Y + B.Y };
-
-        Vector D = A.Add(B);
-
-        //Сложение векторов для типа Vector
-        //1
-        Vector E = A + B;
-        //2
-        Vector F = + A;
-        //3
-        Vector G = A + (3, 3);
-
-        Console.ReadLine();
     }
 }
