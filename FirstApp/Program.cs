4f4eebb231c71ac6a970891f4704e498f925bba0
sTorov <sotorov2011@yandex.ru>
2022-06-28 00:32:42 +0300

    Задание 7.5.2
    Создайте класс Obj, определите в нем поля Name, Description (тип строки) и статическое поле MaxValue типа int, равное 2000.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,36 +1,8 @@
-﻿class SimpleClass
+﻿class Obj
 {
-    public static int MinValue = 100;
-
-    public int Value;
-
-    public SimpleClass(int value)
-    {
-        if (value < MinValue)
-        {
-            Value = MinValue;
-        }
-        else
-        {
-            Value = value;
-        }
-    }
+    public static int MaxValue = 2000;
+    
+    public string Name;
+    public string Description;
 }
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        SimpleClass simpleClass = new(200);
-        SimpleClass simpleClass2 = new(30);
-
-        /*
-         * Для обращения к статическому элементу вне класса, стоит обращаться к нему через класс, 
-         * в котором он располагается, в нашем случае — SimpleClass.
-         */
-
-        Console.WriteLine(SimpleClass.MinValue);
-
-        Console.ReadKey();
-    }
-}
