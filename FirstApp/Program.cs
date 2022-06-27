04c5d108b1db159715aa7eb3a533d129f0ca9e9a
sTorov <sotorov2011@yandex.ru>
2022-06-28 01:03:38 +0300

    Статические поля, свойства и методы
    Обращение к статическим полям, свойствам и методам

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,8 +1,46 @@
-﻿class Obj
+﻿/*
+ * Переменные и свойства, которые хранят состояние, общее для всех объектов класса, 
+ * следует определять как статические.
+ * 
+ * Важно понимать, что статические методы могут обращаться только к статическим членам
+ * класса и переданным им параметрам. Обращаться к нестатическим методам, полям или 
+ * свойствам внутри них невозможно. Также, статический метод нельзя переопределить 
+ * или объявить виртуальным.
+ */
+
+class Counter
 {
-    public static int MaxValue = 2000;
-    
-    public string Name;
-    public string Description;
+    private static int increasingCounter;
+    public static int IncreasingCounter
+    {
+        get
+        {
+            return increasingCounter;
+        }
+        set
+        {
+            if (value > increasingCounter)
+            {
+                increasingCounter = value;
+            }
+        }
+    }
 }
 
+class Helper
+{
+    public static int Sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Counter.IncreasingCounter++;
+        Console.WriteLine(Counter.IncreasingCounter);
+
+        int value = Helper.Sum(1, 2, 3);
+    }
+}
