1832701ffb279ebc43a7d52fd47d78fbfc783b1d
sTorov <sotorov2011@yandex.ru>
2022-06-28 17:46:44 +0300

    Статический конструктор

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,22 +1,42 @@
-﻿class Helper
+﻿/*
+ * Статические конструкторы имеют следующие отличительные черты:
+
+
+Статические конструкторы не могут иметь модификатора доступа.
+
+Статические конструкторы не могут принимать параметры.
+
+Статические конструкторы не могут использовать нестатические поля (и не могут 
+использовать и ключевое слово this).
+
+Статический конструктор невозможно вызвать в коде.
+
+Статический конструктор выполняется при первом создании экземпляра данного класса, 
+либо при первом обращении к статическим членам класса.
+ */
+
+class Car
 {
-    public static void Swap(ref int num1,ref int num2)
+    public static int MinPrice = 100_000;
+    public static int MaxPrice;
+
+    static Car()
     {
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
+        Console.WriteLine("Вызван статический конструктор класса Car");
+        MaxPrice = 1_000_000;
     }
+
+    public int Price;
 }
 class Program
 {
     static void Main(string[] args)
     {
-        int num1 = 3;
-        int num2 = 58;
+        Car car = new Car();
 
-        Helper.Swap(ref num1,ref num2);
+        Console.WriteLine(Car.MinPrice);
+        Console.WriteLine(Car.MaxPrice);
 
-        Console.WriteLine(num1); //58
-        Console.WriteLine(num2); //3
+        Console.ReadLine();
     }
 }
