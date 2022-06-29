d1b5ce60490b1871d05b3b7f6c200921d37dedb3
sTorov <sotorov2011@yandex.ru>
2022-06-30 00:19:58 +0300

    Наследовании обобщений при их ограничении

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,10 +1,12 @@
-﻿//Также можно создавать класс-наследник как обобщение, у которого будет свой собственный тип:
+﻿//При этом стоит учитывать, что если на уровне базового класса для универсального параметра установлено
+//ограничение, то подобное ограничение должно быть определено и в производных классах, которые также используют
+//этот параметр:
 
-class BaseClass<T>
+class BaseClass<T> where T : struct
 {
     public T Field;
 }
-class DerivedClass<T> : BaseClass<int>
+class DerivedClass<T> : BaseClass<T> where T : struct
 {
     public T Property { get; set; }
 }
@@ -13,12 +15,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        DerivedClass<string> derived = new DerivedClass<string>();
-        derived.Field = 13;         // Тип int
-        derived.Property = "22";	// Тип string
-
         DerivedClass<long> derived2 = new DerivedClass<long>();
-        derived2.Field = 11;        // Тип int
+        derived2.Field = 11;        // Тип long
         derived2.Property = 23;     // Тип long
     }
 }
