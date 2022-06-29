15b06be084ec981ff490673b37e9c74913214765
sTorov <sotorov2011@yandex.ru>
2022-06-29 23:19:57 +0300

    Задание 7.6.9
    Установите ограничения на универсальные типы в классе Car. Такие, чтобы поле Engine могло принимать тип ElectricEngine и GasEngine, а параметр newPart метода ChangePart мог бы принимать только типы частей машины (Battery, Differential, Wheel).

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,70 +1,18 @@
-﻿//Теория.txt =>>>...
-class Order<TDelivery> where TDelivery : Delivery
+﻿class Car<TEngine> where TEngine : Engine
 {
-	Delivery Delivery = new Delivery();
-	
-	public int Number;
+    public TEngine Engine;
 
-	public string Description;
-
-	// ... Другие поля	
-
-	public void DisplayAddress()
+    public virtual void ChangePart<TPart>(TPart newPart) where TPart : CarPart 
     {
-        Console.WriteLine(Delivery.Address);
-    }
-}
-class Delivery
-{
-	public string Address;
+        
+    } 
 }
 
-class HomeDelivery : Delivery { /* ... */ }
-class PickPointDelivery : Delivery { /* ... */ }
-class ShopDelivery : Delivery { /* ... */  }
+abstract class CarPart { }
+abstract class Engine { }
 
-
-//Обобщённый класс в качестве ограничения
-//Важно, что только один класс может использоваться в качестве ограничения универсального параметра
-class PremiumService<T> where T : Order<HomeDelivery>
-{
-	public T Order;
-}
-
-
-//Если класс использует несколько универсальных параметров, то последовательно можно
-//задать ограничения к каждому из них:
-class Order<TDelivery, TStruct>
-	where TDelivery : Delivery
-	where TStruct : struct
-{
-	public TDelivery Delivery;
-
-	public TStruct Struct;
-
-	// ... Другие поля
-
-
-	//Для методов синтаксис ограничений является точно таким же:
-	public static T Return<T>(int number) where T : new()
-	{
-		Console.WriteLine(number);
-
-		return new T();
-	}
-}
-
-
-
-class Program
-{
-	static void Main(string[] args)
-    {
-		//Работает
-		Order<HomeDelivery> order = new Order<HomeDelivery>();
-        //Ошибка
-        Order<int> order2 = new Order<int>();
-
-        order.DisplayAddress();
-    }
-}
+class Battery : CarPart { }
+class Differential : CarPart { }
+class Wheel : CarPart { }
+class ElectricEngine : Engine{ }
+class GasEngine : Engine{ }
