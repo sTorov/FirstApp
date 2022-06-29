e120ae10d38c257ce8350c6f218b8270e678a462
sTorov <sotorov2011@yandex.ru>
2022-06-29 20:09:40 +0300

    Задание 7.6.2
    Создайте класс-обобщение Car для автомобиля. Универсальным параметром будет тип двигателя в автомобиле (электрический и бензиновый). Для типов двигателей также создайте классы — ElectricEngine и GasEngine.
    
    В классе Car создайте поле Engine в качестве типа которому укажите универсальный параметр.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,26 +1,12 @@
-﻿/*
- * Обобщённый тип — это тип, который имеет параметр, в качестве которого может 
- * выступать другой тип данных. Такой параметр также называется универсальным параметром.
- */
-
-class Order<T>
+﻿class Car<T>
 {
-	public T Number;
-	public string Description;
-
-	//...
+    public T Engine;
 }
-class Program
+class ElectricEngine
 {
-	static void Main(string[] args)
-    {
-		Order<int> order1 = new Order<int>();
-		Order<string> order2 = new Order<string>();
 
-		order1.Number = 1234;
-		order2.Number = "NN-1234";
+}
+class GasEngine
+{
 
-		int number1 = order1.Number;
-		string number2 = order2.Number;
-	}
 }
