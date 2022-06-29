4f3f438e687c7563070b422363cf0ae5dedeb624
sTorov <sotorov2011@yandex.ru>
2022-06-29 23:05:44 +0300

    Ограничение обобщений
    Конкретизация обобщений
    Много информации

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,11 +1,70 @@
-﻿class Car<T1>
+﻿//Теория.txt =>>>...
+class Order<TDelivery> where TDelivery : Delivery
 {
-    public T1 Engine;
+	Delivery Delivery = new Delivery();
+	
+	public int Number;
 
-    public virtual void ChangePart<T2>(T2 newPart) { }
+	public string Description;
+
+	// ... Другие поля	
+
+	public void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
 }
-class Battery { }
-class Differential { }
-class Wheel { }
-class ElectricEngine { }
-class GasEngine { }
+class Delivery
+{
+	public string Address;
+}
+
+class HomeDelivery : Delivery { /* ... */ }
+class PickPointDelivery : Delivery { /* ... */ }
+class ShopDelivery : Delivery { /* ... */  }
+
+
+//Обобщённый класс в качестве ограничения
+//Важно, что только один класс может использоваться в качестве ограничения универсального параметра
+class PremiumService<T> where T : Order<HomeDelivery>
+{
+	public T Order;
+}
+
+
+//Если класс использует несколько универсальных параметров, то последовательно можно
+//задать ограничения к каждому из них:
+class Order<TDelivery, TStruct>
+	where TDelivery : Delivery
+	where TStruct : struct
+{
+	public TDelivery Delivery;
+
+	public TStruct Struct;
+
+	// ... Другие поля
+
+
+	//Для методов синтаксис ограничений является точно таким же:
+	public static T Return<T>(int number) where T : new()
+	{
+		Console.WriteLine(number);
+
+		return new T();
+	}
+}
+
+
+
+class Program
+{
+	static void Main(string[] args)
+    {
+		//Работает
+		Order<HomeDelivery> order = new Order<HomeDelivery>();
+        //Ошибка
+        Order<int> order2 = new Order<int>();
+
+        order.DisplayAddress();
+    }
+}
