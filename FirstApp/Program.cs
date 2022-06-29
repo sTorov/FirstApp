2e0d7cc1f3c3f208e903a5159222b3585431be5b
sTorov <sotorov2011@yandex.ru>
2022-06-29 20:40:27 +0300

    Задание 7.6.6
    Реализуйте класс-обобщение Record, у которого будут два универсальных параметра: один — для идентификатора записи (Id), другой — для значения записи (Value). Также в классе реализуйте поле Date типа DateTime.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,19 +1,6 @@
-﻿class Generic<T1, T2, T3>
+﻿class Record<T1, T2>
 {
-	public T1 Field; // Поле типа T1
-
-	public T2 Property { get; set; } // Свойство типа T2
-
-	public void Method(T3 param) // Параметр типа T3
-	{
-
-	}
-}
-
-class Program
-{
-	static void Main(string[] args)
-    {
-		Generic<object, int, string> generic = new Generic<object, int, string>();
-    }
+    public T1 Id;
+    public T2 Value;
+    public DateTime Date;
 }
