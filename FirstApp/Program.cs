b95cb7cfa38a585b60495326e7320b1416ef0c88
sTorov <sotorov2011@yandex.ru>
2022-07-02 00:44:35 +0300

    Задание 8.1.5
    Сейчас пользователь видит, что у  вас на диске все файлы лежат в одной куче.
    
    Нужно создать папки (директории) для сортировки файлов.
    
    Добавьте метод для создания новой директории на диске. Подумайте, какую коллекцию использовать для хранения созданных директорий.
    
    Реализация метода:
    
    Принимает на вход имя папки, добавляет её в коллекцию папок, выводит информацию о том, что папка создана.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,4 +10,15 @@
         TotalSpace = totalSpace;
         FreeSpace = freeSpace;
     }
+
+    Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+    public void CreateFolder(string name)
+    {
+        Folders.Add(name, new Folder());
+    }
+}
+public class Folder
+{
+    public List<string> Files { get; set; } = new List<string>();
 }
