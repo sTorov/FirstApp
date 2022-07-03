e583eddaf599fb6339a8b917c0c31d939a9dfa53
sTorov <sotorov2011@yandex.ru>
2022-07-04 00:00:05 +0300

    Задание 8.2.3
    Добавьте в задание 8.2.2 удаление вновь созданной директории и проверьте: теперь ваша программа не должна оставлять после себя следов!

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,26 +10,35 @@ namespace DriverManager
             Console.OutputEncoding = System.Text.Encoding.UTF7;
             Console.InputEncoding = System.Text.Encoding.UTF7;
 
-            DeleteFolder();
+            GetAmountOfElements();
 
             Console.ReadLine();
         }
-        static void DeleteFolder()
+        static void GetAmountOfElements()
         {
             try
             {
-                DirectoryInfo dir = new DirectoryInfo("C:\\CSharpDir");
-                if(!dir.Exists)
-                    dir.Create();
+                DirectoryInfo dirInfo = new DirectoryInfo("C:\\");
+                if(dirInfo.Exists)
+                    Console.WriteLine($"Папки: {dirInfo.GetDirectories().Length}\nФайлы: {dirInfo.GetFiles().Length}");
+                
+                DirectoryInfo newDir = new DirectoryInfo("C:\\DirCSharp");
+                if (!newDir.Exists)
+                    newDir.Create();
 
-                dir.CreateSubdirectory("NewDir");
+                Console.WriteLine($"\nНазвание каталога: {newDir.Name}");
+                Console.WriteLine($"Полное название каталога: {newDir.FullName}");
+                Console.WriteLine($"Время создания каталога: {newDir.CreationTime}");
+                Console.WriteLine($"Корневой каталог: {newDir.Root}");
 
-                dir.Delete(true);
-                Console.WriteLine("Папка удалена");
+                Console.WriteLine($"\nПапки: {dirInfo.GetDirectories().Length}\nФайлы: {dirInfo.GetFiles().Length}\n");
+
+                newDir.Delete();
+                Console.WriteLine($"{newDir.Name} удалена");
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine(e.Message);
             }
         }
     }
