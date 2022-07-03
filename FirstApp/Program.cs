fec8286383b027164b525f7df9ecb33c090d4c33
sTorov <sotorov2011@yandex.ru>
2022-07-03 22:56:01 +0300

    Классы Directory и DirectoryInfo
    Позволяют получить информацию о файлах и папках

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,22 +10,29 @@ namespace DriverManager
             Console.OutputEncoding = System.Text.Encoding.UTF7;
             Console.InputEncoding = System.Text.Encoding.UTF7;
 
-            DriveInfo[] drives = DriveInfo.GetDrives();
-
-            foreach (DriveInfo drive in drives)
-            {
-                Console.WriteLine($"Название: {drive.Name}");
-                Console.WriteLine($"Тип: {drive.DriveType}");
-                if(drive.IsReady)
-                {
-                    Console.WriteLine($"Объём: {drive.TotalSize}");
-                    Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
-                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
-                }
-                Console.WriteLine();
-            }
+            GetCatalogs();
 
             Console.ReadLine();
+        }
+        static void GetCatalogs()
+        {
+            string dirName = "C:\\";
+            if (Directory.Exists(dirName))  //Проверяем наличие папки
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  //Получаем все папки корневого каталога
+
+                foreach (var dir in dirs)   //Выведим их все в консоль
+                    Console.WriteLine(dir);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName); //Получим все файлы корневого каталога
+
+                foreach (var file in files)
+                    Console.WriteLine(file);                
+            }
         }
     }
 }
