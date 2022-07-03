36e79a4baffa533347f53544384aaadb3b2d682d
sTorov <sotorov2011@yandex.ru>
2022-07-03 23:09:08 +0300

    Задание 8.2.1
    Напишите метод, который считает количество файлов и папок в корне вашего диска и выводит итоговое количество объектов.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,28 +10,21 @@ namespace DriverManager
             Console.OutputEncoding = System.Text.Encoding.UTF7;
             Console.InputEncoding = System.Text.Encoding.UTF7;
 
-            GetCatalogs();
+            GetAmountOfElements();
 
             Console.ReadLine();
         }
-        static void GetCatalogs()
+        static void GetAmountOfElements()
         {
-            string dirName = "C:\\";
-            if (Directory.Exists(dirName))  //Проверяем наличие папки
+            try
             {
-                Console.WriteLine("Папки:");
-                string[] dirs = Directory.GetDirectories(dirName);  //Получаем все папки корневого каталога
-
-                foreach (var dir in dirs)   //Выведим их все в консоль
-                    Console.WriteLine(dir);
-
-                Console.WriteLine();
-
-                Console.WriteLine("Файлы:");
-                string[] files = Directory.GetFiles(dirName); //Получим все файлы корневого каталога
-
-                foreach (var file in files)
-                    Console.WriteLine(file);                
+                DirectoryInfo dirInfo = new DirectoryInfo("C:\\");
+                if(dirInfo.Exists)
+                    Console.WriteLine($"Папки: {dirInfo.GetDirectories().Length}\nФайлы: {dirInfo.GetFiles().Length}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
