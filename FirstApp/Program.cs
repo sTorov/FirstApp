7ed4b25a29120ee296393484644726a8eea03a5c
sTorov <sotorov2011@yandex.ru>
2022-07-04 23:24:57 +0300

    Задание 8.3.2
    Сделайте так, чтобы ваша программа из задания 8.3.1 при каждом запуске добавляла в свой исходный код комментарий о времени последнего запуска.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,48 +4,19 @@ class FileWriter
 {
     public static void Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF7;
-        Console.InputEncoding = System.Text.Encoding.UTF7;
-        
-        string tempFile = Path.GetTempFileName();   // используем генерацию имени файла.
-        var fileInfo = new FileInfo(tempFile);      // Создаем объект класса FileInfo.
-
-        //Создаем файл и записываем в него.
-        using (StreamWriter sw = fileInfo.CreateText())
+        var fileInfo = new FileInfo(@"D:\VisualStudio\repos\FirstApp\FirstApp\Program.cs");
+                
+        using (StreamWriter sw = fileInfo.AppendText())
         {
-            sw.WriteLine("Строка 1");
-            sw.WriteLine("Строка 2");
-            sw.WriteLine("Строка 3");
+            sw.WriteLine($"//Время запуска: {DateTime.Now}");
         }
-
-        //Открываем файл и читаем из него.
+        
         using (StreamReader sr = fileInfo.OpenText())
         {
             string str = "";
             while ((str = sr.ReadLine()) != null)
                 Console.WriteLine(str);
-        }
-
-        try
-        {
-            string tempFile2 = Path.GetTempFileName();  
-            var fileInfo2 = new FileInfo(tempFile2);
-
-            // Убедимся, что файл назначения точно отсутствует
-            fileInfo2.Delete();
-
-            // Копируем информацию
-            fileInfo.CopyTo(tempFile2);
-            Console.WriteLine($"{tempFile} скопирован в {tempFile2}");
-
-            //Удаляем ранее созданный файл.
-            fileInfo.Delete();
-            Console.WriteLine($"{tempFile} удалён");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"Ошибка: {e}");
-        }
+        }        
 
         Console.ReadLine();
     }
