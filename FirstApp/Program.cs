5c272df773032f4996ced9141d81b80ce49a3ad0
sTorov <sotorov2011@yandex.ru>
2022-07-04 22:55:44 +0300

    Далее рассмотрим пример аналогичной задачи, где после операций чтения/записи в файл нам необходимо работать с ним в дальнейшем. Для этого используем функционал класса FileInfo.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,14 +4,48 @@ class FileWriter
 {
     public static void Main()
     {
-        string filePath = @"D:\VisualStudio\repos\FirstApp\FirstApp\Program.cs";
-                
-        using (StreamReader sr = File.OpenText(filePath))
+        Console.OutputEncoding = System.Text.Encoding.UTF7;
+        Console.InputEncoding = System.Text.Encoding.UTF7;
+        
+        string tempFile = Path.GetTempFileName();   // используем генерацию имени файла.
+        var fileInfo = new FileInfo(tempFile);      // Создаем объект класса FileInfo.
+
+        //Создаем файл и записываем в него.
+        using (StreamWriter sw = fileInfo.CreateText())
+        {
+            sw.WriteLine("Строка 1");
+            sw.WriteLine("Строка 2");
+            sw.WriteLine("Строка 3");
+        }
+
+        //Открываем файл и читаем из него.
+        using (StreamReader sr = fileInfo.OpenText())
         {
             string str = "";
             while ((str = sr.ReadLine()) != null)
                 Console.WriteLine(str);
-        }        
+        }
+
+        try
+        {
+            string tempFile2 = Path.GetTempFileName();  
+            var fileInfo2 = new FileInfo(tempFile2);
+
+            // Убедимся, что файл назначения точно отсутствует
+            fileInfo2.Delete();
+
+            // Копируем информацию
+            fileInfo.CopyTo(tempFile2);
+            Console.WriteLine($"{tempFile} скопирован в {tempFile2}");
+
+            //Удаляем ранее созданный файл.
+            fileInfo.Delete();
+            Console.WriteLine($"{tempFile} удалён");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ошибка: {e}");
+        }
 
         Console.ReadLine();
     }
