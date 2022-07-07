34b675c3837cc8060ec34dee93ab9af3c40cce09
sTorov <sotorov2011@yandex.ru>
2022-07-07 23:44:15 +0300

    Классы для работы с бинарными данными

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,22 +2,53 @@
 using System.IO;
 class FileWriter
 {
-    public static void Main()
-    {
-        var fileInfo = new FileInfo(@"D:\VisualStudio\repos\FirstApp\FirstApp\Program.cs");
-                
-        using (StreamWriter sw = fileInfo.AppendText())
-        {
-            sw.WriteLine($"//Время запуска: {DateTime.Now}");
-        }
-        
-        using (StreamReader sr = fileInfo.OpenText())
-        {
-            string str = "";
-            while ((str = sr.ReadLine()) != null)
-                Console.WriteLine(str);
-        }        
+    const string SettingsFileName = "Settings.cfg";
 
-        Console.ReadLine();
+    static void Main()
+    {
+        //Пишем
+        WriteValues();
+        //Считываем
+        ReadValues();
+    }
+
+    static void WriteValues()
+    {
+        // Создаем объект BinaryWriter и указываем, куда будет направлен поток данных
+        using (BinaryWriter writer = new BinaryWriter(File.Open(SettingsFileName, FileMode.Create)))
+        {
+            // записываем данные в разном формате
+            writer.Write(20.666F);
+            writer.Write(@"Тестовая строчка");
+            writer.Write(55); 
+            writer.Write(false);
+        }
+    }
+    static void ReadValues()
+    {
+        float FloatValues;
+        string StringValues;
+        int IntValues;
+        bool BoolValues;
+
+        if(File.Exists(SettingsFileName))
+        {
+            // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open
+            using(BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
+            {
+                // Применяем специализированные методы Read для считывания соответствующего типа данных
+                FloatValues = reader.ReadSingle();
+                StringValues = reader.ReadString();
+                IntValues = reader.ReadInt32();
+                BoolValues = reader.ReadBoolean();
+            }
+
+            Console.WriteLine("Из файла считано:");
+
+            Console.WriteLine("Дробь " + FloatValues);
+            Console.WriteLine("Строка " + StringValues);
+            Console.WriteLine("Целое " + IntValues);
+            Console.WriteLine("Булево значение " + BoolValues);
+        }
     }
 }
