a0a0bb78f4d7135a14da76e69f5c7b47ac06e2d7
sTorov <sotorov2011@yandex.ru>
2022-07-08 00:31:02 +0300

    Бинарная сериализация
    Теперь, когда мы рассмотрели, как сохранять и считывать информацию с текстовых и бинарных файлов, нужно определиться, как эти данные преобразовывать для удобного представления в вашей программе.
    
    Для этого С# предоставляет механизм бинарной сериализации. Она служит для преобразования объекта в поток байтов для последующей удобной записи в файл или хранения в памяти.
    
    Впоследствии можно выполнить обратный процесс — десериализацию (преобразовать массив байт в ранее сохраненный объект).
    
    Если вам нужно, чтобы определенное поле или свойство при сериализации игнорировалось, его вы помечаете атрибутом NonSerialized.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,37 +1,16 @@
-﻿using System;
-using System.IO;
-class FileWriter
+﻿[Serializable] //   Атрибут сериализации
+class Person
 {
-    static void Main()
+    //     Простая модель класса 
+    [NonSerialized]
+    private string name;
+    public string Name { get { return name; } set { name = value; } }
+    public int Year { get; set; }
+
+    // Метод - конструктор
+    public Person(string name, int year)
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF7;
-        Console.InputEncoding = System.Text.Encoding.UTF7;
-
-        string FileName = @"C:\Users\1357680\Desktop\BinaryFile.bin";
-
-        WriteValues(FileName);
-        ReadValues(FileName);
-    }
-
-    static void WriteValues(string path)
-    {
-        using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
-        {
-            writer.Write("Файл изменён " + DateTime.Now + " на компьютере ОС " + Environment.OSVersion);
-        }
-    }
-    static void ReadValues(string path)
-    {
-        string StringValues;
-
-        if(File.Exists(path))
-        {
-            using(BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
-            {
-                StringValues = reader.ReadString();
-            }
-
-            Console.WriteLine(StringValues);
-        }
+        Name = name;
+        Year = year;
     }
 }
