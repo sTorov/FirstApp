a5b0cc3a28614a63ffb093a1f9da07430a79b728
sTorov <sotorov2011@yandex.ru>
2022-07-09 22:34:51 +0300

    Юнит 9.2 Исключения TryCatchFinally

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,9 +1,30 @@
 ﻿class Program
 {
+    static int Division(int a, int b)
+    {
+        return a / b;
+    }
+
     static void Main()
     {
-        Exception exception = new Exception("Ошибка");
+        try
+        {
+            var result = Division(7, 0);
 
-        exception.HelpLink = "www.yandex.ru";
+            Console.WriteLine(result);
+        }
+        catch (Exception ex)
+        {
+            if (ex is DivideByZeroException)
+                Console.WriteLine("На ноль делить нельзя" + ex.Data);
+            else
+                Console.WriteLine("Произошла непредвиденная ошибка");
+        }
+        finally
+        {
+            Console.WriteLine("Сработал блок finally");
+        }
+
+        Console.ReadLine();
     }
 }
