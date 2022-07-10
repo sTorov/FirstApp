be4b420680d5533fc749a7e806bcb1db9a49cb3d
sTorov <sotorov2011@yandex.ru>
2022-07-10 21:17:33 +0300

    Объединение делегатов

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,28 +1,34 @@
-﻿namespace DelegatePractices
+﻿class Program
 {
-    class Program
+    delegate void ShowDelegate();
+    static void Main()
     {
-        delegate void CalculateDelegate(int a, int b);
-        static void Main(string[] args)
-        {
-            CalculateDelegate calcDelegate = CalculateOne;
+        ShowDelegate show1 = Method1;
+        show1 += Method3;
 
-            calcDelegate += CalculateTwo;
-            calcDelegate -= CalculateTwo;
+        ShowDelegate show2 = Method2;
+        show2 += Method4;
 
-            calcDelegate.Invoke(100, 30);
+        ShowDelegate show3 = show1 + show2;
 
-            Console.Read();
-        }
-
-        static void CalculateOne(int a, int b)
-        {
-            Console.WriteLine(a - b);
-        }
-
-        static void CalculateTwo(int a, int b)
-        {
-            Console.WriteLine(a + b);
-        }
+        show3.Invoke();
     }
+
+    static void Method1()
+    {
+        Console.WriteLine("Метод 1");
+    }
+    static void Method2()
+    {
+        Console.WriteLine("Метод 2");
+    }
+    static void Method3()
+    {
+        Console.WriteLine("Метод 3");
+    }
+    static void Method4()
+    {
+        Console.WriteLine("Метод 4");
+    }
+
 }
