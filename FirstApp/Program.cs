60cb6acf76c0e4d7a3b8a91a2b9483767acd1ebc
sTorov <sotorov2011@yandex.ru>
2022-07-10 23:00:34 +0300

    Передача делегата в качестве параметра для метода

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,15 +1,59 @@
-﻿namespace DelegatePractices
+﻿using static DelegatePractices.Program;
+
+namespace DelegatePractices
 {
-    class Program
+    public class Program
     {
-        delegate int RandomNumberDelegate();
+        public delegate bool PowerUpEmployee(Employee employee);
         static void Main(string[] args)
         {
-            RandomNumberDelegate randomNumberDelegate = () => { return new Random().Next(0, 100); };
+            Employee employee1 = new Employee()
+            {
+                Id = 44,
+                Name = "Dima",
+                Salary = 25000,
+                Expirience = 5,
+            };
+            Employee employee2 = new Employee()
+            {
+                Id = 45,
+                Name = "Ivan",
+                Salary = 10000,
+                Expirience = 2,
+            };
 
-            int result = randomNumberDelegate.Invoke();
-            Console.WriteLine(result);
-            Console.Read();
+            PowerUpEmployee powerUpEmployee = PowerUp;
+
+            List<Employee> listEmployees = new List<Employee>();
+            listEmployees.Add(employee1);
+            listEmployees.Add(employee2);
+
+            Employee.PromoEmploy(listEmployees, powerUpEmployee);
+        }
+
+        static bool PowerUp(Employee employee)
+        {
+            if (employee.Salary > 10000)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Expirience { get; set; }
+
+        public static void PromoEmploy(List<Employee> employees, PowerUpEmployee powerUpEmployee)
+        {
+            foreach (var employee in employees)
+            {
+                if(powerUpEmployee.Invoke(employee))
+                    Console.WriteLine($"Сотрудник {employee.Name} повышен");
+            }
         }
     }
 }
