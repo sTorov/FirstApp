7414d5f3a06338b3efa049d9826a1fc2515a1481
sTorov <sotorov2011@yandex.ru>
2022-07-10 22:09:00 +0300

    Лямбда-выражения

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,15 +2,15 @@
 {
     class Program
     {
-        delegate int RandomNumberDelegate();
+        delegate int Calculate(int a, int c);
+        delegate void Print();
         static void Main(string[] args)
         {
-            RandomNumberDelegate randomNumberDelegate = delegate ()
-            {
-                return new Random().Next(0, 100);
-            };
-            int result = randomNumberDelegate.Invoke();
-            Console.WriteLine(result);
+            Calculate calculation = (x, y) => x + y;
+            Console.WriteLine(calculation(10, 20));
+            Console.WriteLine(calculation(40, 20));
+            Print print = () => Console.WriteLine("Hello world");
+            print.Invoke();
             Console.Read();
         }
     }
