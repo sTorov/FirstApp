632c904b5733a4a1d2e2747351fd80d8ad6d5537
sTorov <sotorov2011@yandex.ru>
2022-07-13 23:08:27 +0300

    Ковариация
    Ковариация — это когда мы можем объявлять делегат и назначать ему методы другой сигнатуры, но которые являются производными от основного метода.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,20 +1,22 @@
-﻿class Program
+﻿class Car
 {
+    public string Model { get; set; }
+}
+class BMW : Car { }
+
+class Program
+{
+    delegate Car CarDelegate (string name);
     static void Main()
     {
-        try
-        {
-            throw new RankException();
-        }
-        catch (RankException ex)
-        {
-            Console.WriteLine(ex.GetType());
-        }
-        finally
-        {
-            Console.Read();
-        }
-
+        CarDelegate carDelegate;
+        carDelegate = BuildBMW;     //ковариантность
+        Car c = carDelegate("X6");
+        Console.WriteLine(c.Model);
         Console.ReadLine();
+    }
+    private static BMW BuildBMW(string model)
+    {
+        return new BMW { Model = model};
     }
 }
