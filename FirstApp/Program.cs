2393a675991eccdce8abd9c9b7e20161cfc58fce
sTorov <sotorov2011@yandex.ru>
2022-07-14 00:27:51 +0300

    События. Пример

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,4 +2,28 @@
 public class ProcessBusinessLogic
 {
     public event Notify ProcessCompleted; // событие
+    public void StartProcess()
+    {
+        Console.WriteLine("Процесс начат!");
+        OnProcessCompleted();
+    }
+    protected virtual void OnProcessCompleted()
+    {
+        ProcessCompleted?.Invoke();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ProcessBusinessLogic bl = new ProcessBusinessLogic();
+        bl.ProcessCompleted += bl_ProcessCompleted;
+        bl.StartProcess();
+    }
+    //Перехватчик событий
+    public static void bl_ProcessCompleted()
+    {
+        Console.WriteLine("Процесс завершен!");
+    }
 }
