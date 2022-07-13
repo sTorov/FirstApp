1a9305bc29fe8bd2ae02cebe7144c9cfb26b309c
sTorov <sotorov2011@yandex.ru>
2022-07-14 00:11:38 +0300

    Объявление события

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,20 +1,5 @@
-﻿class Program
+﻿public delegate void Notify();  // делегат                
+public class ProcessBusinessLogic
 {
-    static void Main()
-    {
-        try
-        {
-            throw new RankException();
-        }
-        catch (RankException ex)
-        {
-            Console.WriteLine(ex.GetType());
-        }
-        finally
-        {
-            Console.Read();
-        }
-
-        Console.ReadLine();
-    }
+    public event Notify ProcessCompleted; // событие
 }
