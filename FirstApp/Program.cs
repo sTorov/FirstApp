b2399987107302b43db89df18c353601f08decd8
sTorov <sotorov2011@yandex.ru>
2022-07-16 19:13:59 +0300

    Dependency Injection Пример

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,23 +1,44 @@
-﻿class Program
+﻿namespace ExampleInterface
 {
-    public interface ILogger        //Объявление интерфейса
+    class Program
     {
-        void Log()
+        static ILogger Logger { get; set; }
+        static void Main()
         {
-            Console.WriteLine("Hello! I am Logger!");
+            Logger = new Logger();
+            var worker1 = new Worker1(Logger);
+            var worker2 = new Worker2(Logger);
+            var worker3 = new Worker3(Logger);
+
+            worker1.Work();
+            worker2.Work();
+            worker3.Work();
+
+            Console.ReadKey();
         }
     }
 
-    public interface IWriter        //Объявление интерфейса
+    public interface ILogger
     {
-        void Write()
+        void Event(string message);
+        void Error(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Error(string message)
         {
-            Console.WriteLine("Hello! I am Writer!");
+            Console.WriteLine(message);
+        }
+
+        public void Event(string message)
+        {            
+            Console.WriteLine(message);
         }
     }
 
-    public class Writer : IWriter , ILogger      //Множественное наследование интерфейсов
-    {                                   
-
+    public interface IWorker
+    {
+        void Work();
     }
 }
---
+++ FirstApp/Worker1.cs
@@ -0,0 +1,50 @@
+﻿namespace ExampleInterface
+{
+    public class Worker1 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker1(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker1 начал свою работу");
+            Thread.Sleep(3000);
+            Logger.Event("Worker1 закончил свою работу");
+        }
+    }
+
+    public class Worker2 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker2(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker2 начал свою работу");
+            Thread.Sleep(3000);
+            Logger.Event("Worker2 закончил свою работу");
+        }
+    }
+
+    public class Worker3 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker3(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker3 начал свою работу");
+            Thread.Sleep(3000);
+            Logger.Event("Worker3 закончил свою работу");
+        }
+    }
+}
