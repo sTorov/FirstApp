be0f0f5380f240e1cdf7d86cfbd845edd79f3211
sTorov <sotorov2011@yandex.ru>
2022-07-21 00:17:22 +0300

    Ковариантный интерфейс

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,18 +1,37 @@
 ﻿namespace InterfacePractices
 {
-    public interface IMessanger<T>              //Обощённый интерфейс
+    class Program
+    {
+        static void Main()
+        {
+            IMessanger<Phone> viberInPhone = new Viber<Phone>();                //Обычное обобщение
+            IMessanger<IPhone> viberInIPhone = new Viber<IPhone>();             //Обычное обобщение
+
+            IMessanger<Phone> viberOutPhone = new Viber<IPhone>();              //Ковариация (более конкретный тип, нежели указан в обобщении)
+
+            viberInPhone.DeviceInfo();
+            viberInIPhone.DeviceInfo();
+
+            Console.ReadLine();
+        }
+    }
+    
+    public interface IMessanger<out T>              //Ковариантный интерфейс !!!<out T>
     {
         T DeviceInfo();
     }
 
-    public class Viber : IMessanger<Phone>
+    public class Viber<TPhone> : IMessanger<TPhone> where TPhone : Phone, new()
     {
-        public Phone DeviceInfo()
+        public TPhone DeviceInfo()
         {
-            throw new NotImplementedException();
+            TPhone device = new TPhone();
+            Console.WriteLine(device);
+            return new TPhone();
         }
     }
 
     public class Phone { }
+    public class IPhone : Phone { }
     public class Computer { }
 }
