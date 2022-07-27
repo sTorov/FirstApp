600d832c6482e52cf1b4e5266a329edc43139aec
sTorov <sotorov2011@yandex.ru>
2022-07-28 00:37:15 +0300

    Разработка Bot.cs

--- TelegramBot/Bot.cs
+++ TelegramBot/Bot.cs
@@ -1,8 +1,14 @@
-﻿using Telegram.Bot;
+﻿using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
 
 namespace TelegramBot
 {
-    class Bot
+    class Bot : BackgroundService
     {
         private ITelegramBotClient _telegarmClient;
 
@@ -10,5 +16,43 @@ namespace TelegramBot
         {
             _telegarmClient = telegramClient;
         }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _telegarmClient.StartReceiving(HandlerUpdateAsync, HandleErrorAsync, new ReceiverOptions() { AllowedUpdates = { } }, cancellationToken: stoppingToken);
+
+            Console.WriteLine("Бот запущен");
+        }
+
+        async Task HandlerUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if(update.Type == UpdateType.CallbackQuery)
+            {
+                await _telegarmClient.SendTextMessageAsync(update.Message.Chat.Id, "Вы нажали кнопку", cancellationToken: cancellationToken);
+                return;
+            }
+
+            if (update.Type == UpdateType.Message)
+            {
+                await _telegarmClient.SendTextMessageAsync(update.Message.Chat.Id, "Вы отправили сообщение", cancellationToken: cancellationToken);
+                return;
+            }
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegarm API Error:\n{apiRequestException.ErrorCode}\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(errorMessage);
+
+            Console.WriteLine("Ожидаем 10 секунд перед повторнрым подключением");
+            Thread.Sleep(10000);
+
+            return Task.CompletedTask;
+        }        
     }
 }
