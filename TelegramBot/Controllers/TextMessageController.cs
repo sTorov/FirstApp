858e0592907a7befd26a1661e33a74eaad7b3cc8
sTorov <sotorov2011@yandex.ru>
2022-07-28 17:26:57 +0300

    Добавление контроллеров

---
+++ TelegramBot/Controllers/TextMessageController.cs
@@ -0,0 +1,21 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot.Controllers
+{
+    class TextMessageController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+
+        public TextMessageController(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Получено текстовое сообщение", cancellationToken: ct);
+        }
+    }
+}
---
+++ TelegramBot/Controllers/VoiceMessageController.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot.Controllers
+{
+    class VoiceMessageController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+
+        public VoiceMessageController(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Получено голосовое сообщение", cancellationToken: ct);
+        }
+    }
+
+}
