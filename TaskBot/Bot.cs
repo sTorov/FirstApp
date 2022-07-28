e98cc4d0702bb65b2baaef0d9c599703c7100048
sTorov <sotorov2011@yandex.ru>
2022-07-28 04:21:23 +0300

    Задание 11.2.5

---
+++ TaskBot/Bot.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace TaskBot
+{
+    class Bot : BackgroundService
+    {
+        private ITelegramBotClient _telegramClient;
+
+        public Bot(ITelegramBotClient botClient)
+        {
+            _telegramClient = botClient;
+        }
+        
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _telegramClient.StartReceiving(HandleUpdateAsync, HandleErrorAsync, new ReceiverOptions { AllowedUpdates = { } }, cancellationToken: stoppingToken);
+
+            Console.WriteLine("Бот запущен");
+        }
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, $"Данный тип сообщения не поддерживается. Отправьте текст!", cancellationToken: cancellationToken);
+                return;
+            }
+
+            if (update.Type == UpdateType.Message)
+            {
+                switch (update.Message!.Type)
+                {
+                    case MessageType.Text:
+                        await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, $"Длина сообщения: {update.Message.Text.Length} знаков", cancellationToken: cancellationToken);
+                        return;
+                    default:
+                        await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, $"Данный тип сообщения не поддерживается. Отправьте текст!", cancellationToken: cancellationToken);
+                        return;
+                }
+            }
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var erroeMessage = exception switch
+            {
+               ApiRequestException apiRequestException => $"Error:\n{apiRequestException.ErrorCode}\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(erroeMessage);
+
+            Console.WriteLine("Ожидание 10 секунд");
+            Thread.Sleep(10000);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TaskBot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+namespace TaskBot
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var host = new HostBuilder().ConfigureServices((hostContext, service) => Configure(service)).UseConsoleLifetime().Build();
+
+            Console.WriteLine("Сервер запущен");
+
+            await host.RunAsync();
+            Console.WriteLine("Сервер остановлен");
+        }
+
+        static void Configure(IServiceCollection services)
+        {
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("5506694649:AAEthtzgiRpdrJCv1l9mBtGshzX6NhnQRtw"));
+            services.AddHostedService<Bot>();
+        }
+    }
+}
