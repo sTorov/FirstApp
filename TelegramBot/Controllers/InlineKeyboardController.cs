e2a44b5214ee40ea5223270893db0198d8d8b71b
sTorov <sotorov2011@yandex.ru>
2022-07-29 04:59:59 +0300

    Обработка нажатия на кнопки

--- TelegramBot/Controllers/InlineKeyboardController.cs
+++ TelegramBot/Controllers/InlineKeyboardController.cs
@@ -1,21 +1,44 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using TelegramBot.Servises;
 
 namespace TelegramBot.Controllers
 {
     class InlineKeyboardController
     {
+        private readonly IStorage _memoryStorage;
         private readonly ITelegramBotClient _telegramClient;
 
-        public InlineKeyboardController(ITelegramBotClient telegramClient)
+        public InlineKeyboardController(ITelegramBotClient telegramClient, IStorage memoryStorage)
         {
             _telegramClient = telegramClient;
+            _memoryStorage = memoryStorage;
         }
 
         public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
         {
+            if(callbackQuery?.Data == null)
+                return;
+
+            _memoryStorage.GetSession(callbackQuery.From.Id).LanguageCode = callbackQuery.Data;
+            
+            string languageText = callbackQuery.Data switch
+            {
+                "ru" => "Русский",
+                "en" => "Английский",
+                _ => String.Empty
+            };
+            
             Console.WriteLine($"Контроллер {GetType().Name} обнаружил нажатие на кнопку");
-            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку", cancellationToken: ct);
+            await _telegramClient.SendTextMessageAsync
+                (
+                    callbackQuery.From.Id, 
+                    $"<b>Язык аудио - {languageText}.{Environment.NewLine}</b>" +
+                    $"{Environment.NewLine}Можно поменять в главном меню.",
+                    cancellationToken: ct,
+                    parseMode: ParseMode.Html
+                );
         }
     }
 }
