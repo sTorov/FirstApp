d4645538cab8e0279d034431fb6e548e56e4d07f
sTorov <sotorov2011@yandex.ru>
2022-07-30 06:17:13 +0300

    Расштрения

---
+++ TelegramBot/Extensions/DirectoryExtensioncs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Extensions
+{
+    public static class DirectoryExtensioncs
+    {
+        /// <summary>
+        /// Получаем путь до каталога с sln. файлом
+        /// </summary>
+        /// <returns></returns>
+        public static string GetSolutionRoot()
+        {
+            var dir = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+            var fullname = Directory.GetParent(dir).FullName;
+            var project = fullname.Substring(0, fullname.Length - 4);
+            return Directory.GetParent(project)?.FullName;
+        }
+    }
+}
---
+++ TelegramBot/Extensions/StringExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Extensions
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Преобразуем строку, чтобы она начаналась с заглавной буквы
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string UppercaseFirst(string s)
+        {
+            if(string.IsNullOrEmpty(s))
+                return string.Empty;
+
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+    }
+}
