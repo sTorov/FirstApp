5e13e7afd5e395b4ee3135a9b229899872dbfaf1
sTorov <sotorov2011@yandex.ru>
2022-08-05 21:15:06 +0300

    Задание 12.1.3
    Попробуйте написать программу (метод) по данному алгоритму.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,9 +2,37 @@
 {
     static void Main()
     {
-        Console.WriteLine("Как Вас зовут?");
-        string name = Console.ReadLine();
-        string greetings = "Привет, " + name;
-        Console.WriteLine(greetings);
+        int count;
+
+        while (true)
+        {
+            Console.WriteLine("Сколько элементов будет в массиве?");
+
+            try
+            {
+                count = int.Parse(Console.ReadLine());
+                if (count <= 0)
+                    throw new Exception("Введённое значение должно быть больше 0!");
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введите число!");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        string[] array = new string[count];
+        for (int i = 0; i < count; i++)
+            array[i] = Console.ReadLine();
+
+        Console.WriteLine("Все элементы записаны");
     }
 }
