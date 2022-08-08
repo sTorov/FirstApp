880022e41bc1787a68b0c6cb3ac31f8e01a8a8c8
sTorov <sotorov2011@yandex.ru>
2022-08-08 03:42:06 +0300

    Рекурсивный бинарный поиск

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,24 +1,51 @@
 ﻿class Program
 {
-	static void GetPageForUser(User user)
+	static void Main()
 	{
-		if (!user.LoggedIn)
-			return;
+		int[] ints = new int[5];
+		Random random = new();
+		int temp = 0;
 
-		Console.WriteLine("User logged in");
+		for(int i = 0; i < ints.Length; i++)
+        {
+			temp = random.Next(0, 10);
+			if (!Array.Exists(ints, s => s == temp))
+            {
+				ints[i] = temp;
+				continue;
+            }
 
-		if (!user.HasAccessToSection)
-			return;
+			i--;
+        }
+		Array.Sort(ints);
 
-		Console.WriteLine("User has access to that section");
+		foreach(var num in ints)
+			Console.Write(num + " ");
+		Console.WriteLine();
 
-		if (user.HasUnseenNews)
-			ShowNews();
-
-
-		if (user.HasAdminRights)
-			DecorateAdminTools();
-
-
+		int result = BinarySearchRecurs(5, ints, 0, ints.Length - 1);
+		string s;
+		Console.WriteLine(s = result != -1 ? "индекс: " + result : "NaN");
+		Console.ReadKey();
 	}
+
+	static int BinarySearchRecurs(int value, int[] array, int left, int right)
+    {
+		int middle = (left + right) / 2;
+		
+		int midElement = array[middle];
+
+		if(midElement == value)
+			return middle;
+
+		if(left < right)
+        {
+			if(value < midElement)
+				return BinarySearchRecurs(value, array, left, middle - 1);
+			
+			return BinarySearchRecurs(value, array, middle + 1, right);
+        }
+
+		return -1;
+    }
 }
