eff8d72767733e36b145f306e22615b28f86e2e2
sTorov <sotorov2011@yandex.ru>
2022-08-11 05:12:20 +0300

    Замер времени выполнения программы при помощи класса StopWatch

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,75 +1,37 @@
-﻿class Program
+﻿using System.Diagnostics;
+
+class Program
 {
     static void Main()
     {
-        int[] ints = new int[] { 1, 3, 3, 5, 6, 7, 7, 10 };
-        int[] ints2 = GetArray(ints, 7);
+        Estimate(20);
+    } 
+    
+    static void CreateMatrix(int n)
+    {
+        var matrix = new int[n][];
 
-        Console.WriteLine(String.Join(", ", ints));
-        Console.WriteLine(String.Join(", ", ints2));
+        for(int i = 0; i < n; i++)
+            matrix[i] = new int[n];
 
-        Console.ReadKey();
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                matrix[i][j] = i + j;
     }
 
-    static int[] GetArray(int[] array, int value)
+    static void Estimate(int n)
     {
-        int[] newArray = new int[array.Length + 1];
-        int index = GetInsertIndex(array, value);
+        var timer = new Stopwatch();
+        timer.Start();
 
-        newArray[index] = value;
+        for(int i = 0; i < n; i++)
+        {
+            timer.Restart();
 
-        for (int i = 0; i < index; i++)
-            newArray[i] = array[i];
-        for (int i = index; i < array.Length; i++)
-            newArray[i + 1] = array[i];
+            CreateMatrix(10000);
 
-        return newArray;
-    }
-
-    static int GetInsertIndex(int[] array, int value)
-    {
-        int subStart = array[0] - value;
-        if (subStart >= 0)
-            return 0;
-
-        int subEnd = array[array.Length - 1] - value;
-        if (subEnd <= 0)
-            return array.Length;
-
-        int mult = 1;
-        int subMid = array[(array.Length - 1) / 2] - value;
-        if (subMid == 0)
-            return (array.Length - 1) / 2;
-        if(subMid < 0)
-            mult = -1;
-
-        if (subStart < subEnd && subStart < subMid * mult)
-            return Front(array, value, 0, (array.Length - 1) / 2);
-        else if (subEnd < subStart && subEnd < subMid * mult)
-            return Back(array, value, array.Length - 1, (array.Length - 1) / 2);
-        else if (subMid < 0)
-            return Front(array, value, (array.Length - 1) / 2, array.Length - 1);
-        else
-            return Back(array, value, (array.Length - 1) / 2, 0);
-
-
-    }
-
-    static int Back(int[] array, int value, int start, int end)
-    {
-        for (int i = start; i >= end; i--)
-            if (array[i] > value)
-                return i;
-
-        return start;
-    }
-
-    static int Front(int[] array, int value, int start, int end)
-    {
-        for (int i = start; i <= end; i++)
-            if (array[i] > value)
-                return i;
-
-        return start;
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+        }
     }
 }
