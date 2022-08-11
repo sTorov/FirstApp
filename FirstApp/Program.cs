90bcc8d86ab02078af3ed5fc5c66a678bb8820d7
sTorov <sotorov2011@yandex.ru>
2022-08-11 06:24:49 +0300

    Bencmark

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,52 +1,10 @@
-﻿using System.Diagnostics;
+﻿using BenchmarkDotNet.Running;
+using FirstApp;
 
 class Program
 {
     static void Main()
     {
-       var array = Estimate(20);
-        MiddleTime(array);
-    } 
-    
-    static void CreateMatrix(int n)
-    {
-        var matrix = new int[n][];
-
-        for(int i = 0; i < n; i++)
-            matrix[i] = new int[n];
-
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n; j++)
-                matrix[i][j] = i + j;
-    }
-
-    static long[] Estimate(int n)
-    {
-        var timer = new Stopwatch();
-        long[] times = new long[n];
-
-        timer.Start();
-
-        for(int i = 0; i < n; i++)
-        {
-            timer.Restart();
-
-            CreateMatrix(10000);
-
-            timer.Stop();
-            times[i] = timer.ElapsedMilliseconds;
-        }
-
-        return times;
-    }
-
-    static void MiddleTime(long[] array)
-    {
-        long sum = 0;
-        for (int i = 0; i < array.Length; i++)
-            sum += array[i];
-
-        Console.WriteLine(sum / array.Length);       
-        
-    }
+        var summary = BenchmarkRunner.Run<Testing>();
+    }      
 }
---
+++ FirstApp/Teasting.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace FirstApp
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class Testing
+    {
+        [Benchmark]
+        public void CreateMatrix()
+        {
+            int n = 10000;
+
+            var matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+                matrix[i] = new int[n];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matrix[i][j] = i + j;
+        }
+    }
+}
