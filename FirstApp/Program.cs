4f40515c1b2c77d4786e9a8698eee4defa3fffb6
sTorov <sotorov2011@yandex.ru>
2022-08-13 16:21:33 +0300

    Задание 3
    Выведите все элементы двемерного массива в консоль построчно.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,9 +2,26 @@
 {
     static void Main()
     {
-        Console.WriteLine("Как Вас зовут?");
-        string name = Console.ReadLine();
-        string greetings = "Привет, " + name;
-        Console.WriteLine(greetings);
+        int[,] myArray =
+        {
+           { 5, 8, 3, 10 },
+           { 13, 2, 1, 7 },
+           { 0, 2, 5, 2 },
+           { 3, 8, 3, 45 },
+           { 2, 4, 31, 4 }
+        };
+
+        PrintArray(myArray);
+    }
+
+    static void PrintArray(int[,] array)
+    {
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+                Console.Write(array[i, j] + "\t");
+
+            Console.WriteLine();
+        }
     }
 }
