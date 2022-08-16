e124e48e9ccba95a3493b6bff337fc7a8138413c
sTorov <sotorov2011@yandex.ru>
2022-08-17 00:28:17 +0300

    Задание 13.3.4
    Напишите метод для нашей телефонной книги, который получает на вход список и новый контакт, и добавляет его, только если он уникален.
    
    После добавления нового контакта метод должен выводить список контактов, отсортированный по имени.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,12 +10,32 @@ class Program
         phoneBook.Add(new Contact("Dima", "dima@gmail.com", 78991234567));
         phoneBook.Add(new Contact("Ivan", "ivan@gmail.com", 78911987654));
 
-        foreach (Contact contact in phoneBook)
-            Console.WriteLine(contact.Name + ": " + contact.PhoneNumber);
+        Console.WriteLine(phoneBook[0].Equals(phoneBook[0]));
+        Console.WriteLine(phoneBook[0].Equals(phoneBook[1]));
+        Console.WriteLine(phoneBook[0].Equals(new object()));
+
+        Console.WriteLine(AddUnique(new Contact("Dima", "dima@gmail.com", 78991234567), phoneBook));
+        Console.WriteLine(AddUnique(new Contact("Ivan", "ivan@gmail.com", 78911987654), phoneBook));
+        Console.WriteLine(AddUnique(new Contact("Anton", "anton@gmail.com", 78991234567), phoneBook));
+        Console.WriteLine(AddUnique(new Contact("Dima", "dima@gmail.com", 71111111111), phoneBook));
+
+    }
+
+    static bool AddUnique(Contact contact, List<Contact> phoneBook)
+    {
+        if (phoneBook.Contains(contact))
+            return false;
+
+        phoneBook.Add(contact);
+        phoneBook.Sort();
+        foreach(var item in phoneBook)
+            Console.WriteLine(item);
+
+        return true;
     }
 }
 
-class Contact
+class Contact : IComparable<Contact>
 {
     public string Name { get; set; }
     public string Email { get; set; }
@@ -26,5 +46,26 @@ class Contact
         Name = name;
         Email = email;
         PhoneNumber = phoneNumber;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if(obj is Contact contact)
+        {
+            if(Name == contact.Name && Email == contact.Email && PhoneNumber == contact.PhoneNumber)
+                return true;
+            return false;
+        }
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return "Имя: " + Name + " E-mail: " + Email + " Телефон: " + PhoneNumber;
+    }
+
+    public int CompareTo(Contact? obj)
+    {
+        return Name.CompareTo(obj?.Name);
     }
 }
