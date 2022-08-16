60b7a725360055dbca22329255a197cb33763bf5
sTorov <sotorov2011@yandex.ru>
2022-08-17 01:51:30 +0300

    Задание 13.3.10
    Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,39 +9,9 @@ class Program
 {
     static void Main()
     {
-        //Создадим два множества
-        SortedSet<char> lettersOne = new SortedSet<char>();
-        SortedSet<char> lettersTwo = new SortedSet<char>();
-
-        //Добавим элементы в первую
-        lettersOne.Add('A');
-        lettersOne.Add('B');
-        lettersOne.Add('C');
-        lettersOne.Add('Z');
-
-        //выведим
-        PrintCollection(lettersOne, "Первая коллекция");
-
-        //Добавим элементы во вторую коллекцию
-        lettersTwo.Add('Y');
-        lettersTwo.Add('X');
-        lettersTwo.Add('Z');
-
-        //выведим
-        PrintCollection(lettersTwo, "Вторая коллекция");
-
-        //Выполним вычитание множеств
-        lettersOne.ExceptWith(lettersTwo);
-
-        //выведим результат
-        PrintCollection(lettersOne, "Результат вычитания");
-    }
-
-    static void PrintCollection(SortedSet<char> chars, string s)
-    {
-        Console.WriteLine(s);
-        foreach (var item in chars)
-            Console.Write(item + " ");
-        Console.WriteLine("\n");
+        string words = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
+        HashSet<char> chars = new HashSet<char>();
+        chars.UnionWith(words);
+        Console.WriteLine(chars.Count);
     }
 }
