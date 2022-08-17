61e9264b6eaee0bd09f293cb93bec93041168251
sTorov <sotorov2011@yandex.ru>
2022-08-17 06:07:54 +0300

    Задания 13.4.5
    Давайте сравним производительность вставки нового значения для Словаря и Сортированного словаря. Для замера производительности используется класс StopWatch из пространства имён System.Diagnostics.

---
+++ FirstApp/Contact.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstApp
+{
+    public class Contact // модель класса
+    {
+        public Contact(long phoneNumber, string email) // метод-конструктор
+        {
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public long PhoneNumber { get; }
+        public string Email { get; }
+
+        public override string ToString()
+        {
+            return "Телефон: " + PhoneNumber + " E-mail: " + Email;
+        }
+    }
+}
---
+++ FirstApp/Dictionary.cs
@@ -0,0 +1,34 @@
+﻿namespace FirstApp
+{
+    class Dictionary
+    {
+        static void Main()
+        {
+            Dictionary<string, Contact> bookPhone = new Dictionary<string, Contact>()
+        {
+            {"Дмитрий", new Contact(123123123, "dima@com.ru") },
+            {"Иван", new Contact(456456456, "ivan@ivan.ru") }
+        };
+
+            foreach (var contact in bookPhone)
+                Console.WriteLine(contact.Key + ": " + contact.Value);
+
+            Console.WriteLine();
+            bookPhone.Add("Василий", new Contact(111222333, "vasya@gmail.com"));
+
+            foreach (var contact in bookPhone)
+                Console.WriteLine(contact.Key + ": " + contact.Value);
+
+            Console.WriteLine();
+            if (bookPhone.ContainsKey("Василий"))
+                bookPhone["Василий"] = new Contact(000111000111, bookPhone["Василий"].Email);
+
+            foreach (var contact in bookPhone)
+                Console.WriteLine(contact.Key + ": " + contact.Value);
+
+            Console.WriteLine();
+        }
+    }
+}
+
+
