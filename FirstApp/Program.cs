34b941fdfd0167e5ae3df3ff2d32e150559c4c87
sTorov <sotorov2011@yandex.ru>
2022-08-20 21:38:45 +0300

    Югит 14 Введение Задание 4
    Задача: выбрать имена на букву А и отсортировать в алфавитном порядке.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,50 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Concurrent;
-using System.Text;
+﻿using System.Text;
 
 class Program
 {
-    //Объявим список в виде статической переменной
-    public static LinkedList<string> strings = new LinkedList<string>();
-
     static void Main(string[] args)
     {
         Console.OutputEncoding = Encoding.UTF7;
         Console.InputEncoding = Encoding.Unicode;
-        
-        //добавим несколько элементов
-        strings.AddFirst("Лиса");
-        strings.AddFirst("Волк");
-        strings.AddFirst("Заяц");
-        var mouse = strings.AddFirst("Мышь");
 
-        GoOnwards();    //прямой проход списка
-        GoBackwards();  //обратный проход списка
+        string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+        List<string> names = new List<string>();
 
-        //вставка нового элемента
-        strings.AddAfter(mouse, "Медведь");
-        
-        Console.WriteLine("\n\nВыведем список ещё раз после вставки\n");
+        foreach (var human in people)
+            if (human.ToUpper().StartsWith("А"))
+                names.Add(human);
 
-        GoOnwards();    //прямой проход списка
-        GoBackwards();  //обратный проход списка
-    }
+        names.Sort();
 
-    static void GoOnwards()
-    {
-        LinkedListNode<string> node;
-
-        Console.WriteLine("Элементы коллекции в прямом направлении:");
-        for(node = strings.First; node != null; node = node.Next)
-            Console.Write(node.Value + " ");
-    }
-
-    static void GoBackwards()
-    {
-        LinkedListNode<string> node;
-
-        Console.WriteLine("\n\nЭлементы коллекции в обратном направлении:");
-        for (node = strings.Last; node != null; node = node.Previous)
-            Console.Write(node.Value + " ");
+        foreach(string name in names)
+            Console.WriteLine(name);
     }
 }
