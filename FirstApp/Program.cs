36cc585d57905f8eb6326763f21efed82d3d8697
sTorov <sotorov2011@yandex.ru>
2022-08-22 04:57:42 +0300

    Задание 14.1.2
    Соедините все слова в одну последовательность (каждое слово — отдельный элемент последовательности).

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,57 +7,19 @@ class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.Unicode;
 
-        //Словарь для хранения стран с городами
-        var Countries = new Dictionary<string, List<City>>();
+        string[] text = 
+        { 
+            "Раз два три",
+            "четыре пять шесть",
+            "семь восемь девять" 
+        };
 
-        //Добавим Россию с её городами
-        var russianCities = new List<City>();
-        russianCities.Add(new City("Москва", 11900000));
-        russianCities.Add(new City("Санкт-Петербург", 4991000));
-        russianCities.Add(new City("Волгоград", 1099000));
-        russianCities.Add(new City("Казань", 1169000));
-        russianCities.Add(new City("Севастополь", 449138));
-        Countries.Add("Россия", russianCities);
+        var words = from str in text //пробегаемся по всем элементам массива
+                    from word in str.Split(' ') //дробим каждый элемент по пробелам, сохраняя в новую последовательность
+                    select word; //собираем все куски в результирующую выборку
 
-        //Добавим Беларусь
-        var belarusCities = new List<City>();
-        belarusCities.Add(new City("Минск", 1200000));
-        belarusCities.Add(new City("Витебск", 362466));
-        belarusCities.Add(new City("Гродно", 368710));
-        Countries.Add("Беларусь", belarusCities);
-
-        //Добавим США
-        var americanCities = new List<City>();
-        americanCities.Add(new City("Нью-Йорк", 8399000));
-        americanCities.Add(new City("Вашингтон", 705749));
-        americanCities.Add(new City("Альбукерке", 560218));
-        Countries.Add("США", americanCities);
-
-        var cities = from country in Countries //пройдёмся по странам
-                     from city in country.Value //пройдёмся по городам
-                     where city.Population > 1000000 //выберем города-милионники
-                     orderby city.Population descending //отсортируем по населению
-                     select city;
-
-        foreach(var city in cities)
-            Console.WriteLine(city);
-    }
-
-    //Создадим модель-класс для города
-    public class City
-    {
-        public string Name { get; set; }
-        public long Population { get; set; }
-
-        public City(string name, long population)
-        {
-            Name = name;
-            Population = population;
-        }
-
-        public override string ToString()
-        {
-            return Name + " - " + Population;
-        }
+        //выводим результат
+        foreach(var word in words)
+            Console.WriteLine(word);
     }
 }
