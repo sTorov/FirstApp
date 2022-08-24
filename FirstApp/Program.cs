edae895907241671a12a49363db9f972678a1d70
sTorov <sotorov2011@yandex.ru>
2022-08-25 00:43:01 +0300

    Задание 14.2.5
    Сделайте вывод контактов в консоль по два в бесконечном цикле.
    
    Выводить нужно постранично, например так: вы ввели 1 — показало Андрея и Сергея, 2 — Ивана и Игоря, 3 — Анну и Василия.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,19 +7,35 @@ class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.Unicode;
 
-        var cars = new[] { "Volvo", "Opel", "Suzuki", "Toyota", "Lada", "Kamaz" };
+        IEnumerable<Contact> page = default;
+        var contacts = new List<Contact>()
+        {
+           new Contact() { Name = "Андрей", Phone = 7999945005 },
+           new Contact() { Name = "Сергей", Phone = 799990455 },
+           new Contact() { Name = "Иван", Phone = 79999675 },
+           new Contact() { Name = "Игорь", Phone = 8884994 },
+           new Contact() { Name = "Анна", Phone = 665565656 },
+           new Contact() { Name = "Василий", Phone = 3434 }
+        };
 
-        //пропустим первые 2 элемента
-        var skip2 = cars.Skip(2);
+        while(true)
+        {
+            char ch = Console.ReadKey(true).KeyChar;
 
-        foreach(var car in skip2)
-            Console.WriteLine(car);
-        Console.WriteLine();
+            if(int.TryParse(ch.ToString(), out int result) && result > 0 && result < 4)
+            {
+                Console.Clear();
+                page = contacts.Skip((result - 1) * 2).Take(2);
 
-        //пропустим первые 2 и добавим следующие 2
-        var skipTake = cars.Skip(2).Take(2);
-
-        foreach(var car in skipTake)
-            Console.WriteLine(car);
+                foreach(var contact in page)
+                    Console.WriteLine(contact.Name + " " + contact.Phone);
+            }
+        }
     }   
+}
+
+public class Contact
+{
+    public string Name { get; set; }
+    public long Phone { get; set; }
 }
