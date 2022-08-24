ca192f444d47160e6e13f02603302898bcd270ea
sTorov <sotorov2011@yandex.ru>
2022-08-25 02:39:24 +0300

    LINQ Множественная сортировка

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -17,27 +17,42 @@ class Program
            new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
         };
 
-        //сортировка по возрастанию
-        var sortStud = from s in students 
-                       orderby s.Age 
-                       select s;
+        //сортировка сначала по возрасту, потом - по имени
+        var sortedStuds = from s in students
+                          orderby s.Age, s.Name
+                          select s;
 
-        //или
-        var sortAsc = students.OrderBy(s => s.Age);
-
-        foreach(var student in sortStud)
+        foreach(var student in sortedStuds)
             Console.WriteLine(student.Name + ", " + student.Age);
         Console.WriteLine();
 
-        //сортировка по убыванию
-        var sortStudDesending = from s in students
-                       orderby s.Age descending
-                       select s;
+        //или
+        var sortedStuds2 = students
+            .OrderBy(s => s.Age)
+            .ThenBy(s => s.Name);
+
+        foreach (var student in sortedStuds2)
+            Console.WriteLine(student.Name + ", " + student.Age);
+        Console.WriteLine();
+
+
+
+
+        //сортировка сначала по имени, потом - по возрасту (убывание)
+        var sortedStudsDesc = from s in students
+                              orderby s.Name descending, s.Age descending
+                              select s;
+
+        foreach (var student in sortedStudsDesc)
+            Console.WriteLine(student.Name + ", " + student.Age);
+        Console.WriteLine();
 
         //или
-        var sortDesc = students.OrderByDescending(s => s.Age);
+        var sortedStuds2Desc = students
+            .OrderByDescending(s => s.Name)
+            .ThenByDescending(s => s.Age);
 
-        foreach (var student in sortStudDesending)
+        foreach (var student in sortedStuds2Desc)
             Console.WriteLine(student.Name + ", " + student.Age);
     }
 }
