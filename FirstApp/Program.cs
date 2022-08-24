811b1ffcb0b1c3fb30ce89d1ddaac2308e38d6c6
sTorov <sotorov2011@yandex.ru>
2022-08-24 23:23:29 +0300

    LINQ ключевое слово let
    генерация полного имени при помощи временной переменной в запросе

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,31 +7,39 @@ class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.Unicode;
 
+        //Подготовим данные
+        List<Student> students = new List<Student>
+        {
+            new Student { Name = "Андрей" , Age =23, Languages = new List<string>{ "английский", "немнцкий" } },
+            new Student { Name = "Сергей" , Age =27, Languages = new List<string>{ "английский", "французский" } },
+            new Student { Name = "Дмитрий" , Age =29, Languages = new List<string>{ "английский", "испанский" } },
+            new Student { Name = "Василий" , Age =24, Languages = new List<string>{ "испанский", "немнцкий" } },
+        };
 
-        string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
+        var fullNameStudents = from s in students
+                               //временная переменная для генерации полного имени
+                               let fullName = s.Name + " Иванов"
+                               //проекция в новую сущность с использованием новой переменой
+                               select new
+                               {
+                                   Name = fullName,
+                                   Age = s.Age
+                               };
 
-        var str = from s in words
-                  orderby s.Length
-                  select new 
-                  { 
-                      Word = s,
-                      Length = s.Length
-                  };
-
-        foreach (var item in str)
-            Console.WriteLine(item.Word + " " + item.Length);
-        Console.WriteLine();
-
-        //или
-        var str2 = words.Select(s => 
-        new 
-        { 
-            Word = s, 
-            Length = s.Length 
-        })
-        .OrderBy(s => s.Length);
-
-        foreach (var item in str2)
-            Console.WriteLine(item.Word + " " + item.Length);
+        foreach(var item in fullNameStudents)
+            Console.WriteLine(item.Name + ", " + item.Age);
     }   
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public List<string> Languages { get; set; }
+}
+
+public class Application
+{
+    public string FirstName { get; set; }
+    public int YearOfBirth { get; set; }
 }
