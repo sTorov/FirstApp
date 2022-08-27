4c83f1f8e1fe4b03cb3a5f88cddc1448a1960232
sTorov <sotorov2011@yandex.ru>
2022-08-28 00:38:04 +0300

    Задание 15.2.1
    Допишите функцию, используя знания LINQ, полученные в этом юните.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,15 +7,32 @@ class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.Unicode;
 
-        int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(Factorial(5));
+        //или
+        Console.WriteLine(Factorial2(5));
+    }
+
+    static long Factorial(int number)
+    {
+        long[] numbers = new long[number];
+        for (int i = 0; i < numbers.Length; i++)
+            numbers[i] = i + 1;
+
+        long result = numbers.Aggregate((x, y) => x * y);
+        return result;
+    }
+
+    //Решение
+    static long Factorial2(int number)
+    {
+        //Коллекция для хранения чисел
+        var numbers = new List<long>();
         
-        int result = numbers.Aggregate((x, y) => x - y);
-        //вычислит 1 - 2 - 3 - 4 - 5 = -13
-        Console.WriteLine(result);
+        //Добавляем все числа от 1 до n в коллекцию
+        for (int i = 1; i <= number; i++)
+            numbers.Add(i);
 
-        int sum = numbers.Aggregate((x, y) => x + y);
-        //вычислит 1 + 2 + 3 + 4 + 5 = 15
-        Console.WriteLine(sum);
-
-    }    
+        //Выполняем агрегацию
+        return numbers.Aggregate((x, y) => x * y);        
+    }
 }
