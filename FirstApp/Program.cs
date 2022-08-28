656c5a432dab5f52818eb2423c88712de5cbf022
sTorov <sotorov2011@yandex.ru>
2022-08-28 04:01:48 +0300

    LINQ Мax(), Min(), Average()
    List<Student>

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,17 +8,55 @@ class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.Unicode;
 
-        var simpleNumbers = new[] { 3, 3, 4 };
+        var nums = new[] { 1, 7, 45, 23 };
 
-        Console.WriteLine(Average(simpleNumbers));
+        int greatest = nums.Max();//вернёт 45
+        int smallest = nums.Min();//вернёт 1
+        double average = nums.Average();//вернёт 19
+
+        Console.WriteLine($"{greatest}, {smallest}, {average}");
+
+        //Сложный тип данных
+
+        // Список студентов
+        List<Student> students = new List<Student>
+        {
+            new Student { Name = "Алёна" , Age =23, Languages = new List<string>{ "английский", "неменцкий" } },
+            new Student { Name = "Яков" , Age =23, Languages = new List<string>{ "английский", "неменцкий" } },
+            new Student { Name = "Андрей" , Age =23, Languages = new List<string>{ "английский", "неменцкий" } },
+            new Student { Name = "Сергей" , Age =27, Languages = new List<string>{ "английский", "французский" } },
+            new Student { Name = "Дмитрий" , Age =29, Languages = new List<string>{ "английский", "испанский" } },
+            new Student { Name = "Василий" , Age =24, Languages = new List<string>{ "испанский", "неменцкий" } },
+        };
+
+        //найдём возраст самого старого студента (29)
+        var oldest = students.Max(s => s.Age);
+
+        //самого молодого (23)
+        var youngest = students.Min(s => s.Age);
+
+        // и средний возраст (26,3)
+        var averageAge = students.Average(s => s.Age);
+
+        Console.WriteLine($"\n{oldest}\n{youngest}\n{averageAge:0.0}");
+
+        //средний возраст студентов, владующих английским языком
+        var avrAgeEngLang = students
+            .Where(s => s.Languages.Contains("английский"))
+            .Average(s => s.Age);
+
+        Console.WriteLine(Environment.NewLine + avrAgeEngLang);
     }
-    static double Average(int[] numbers)
+
+    public class Student
     {
-        return Math.Round((double)numbers.Sum() / numbers.Length, 5);
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Languages { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " - " + Age;
+        }
     }
-}
-public class Student
-{
-    public string Name { get; set; }
-    public int Age { get; set; }        
 }
