e76e703c8c31c5165893eaf4fdf88522dd51ff36
sTorov <sotorov2011@yandex.ru>
2022-08-28 04:53:24 +0300

    Задание 15.2.8
    Напишите программу с бесконечным циклом, как в предыдущем юните, которая будет:
    
    ожидать ввода числа с клавиатуры (используйте Console.ReadLine());
    
    добавлять число в список, хранящийся в памяти;
    
    выводить после добавления следующую информацию: количество чисел в списке, сумму всех чисел списка, наибольшее и наименьшее числа, а также их среднее значение.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,55 +8,33 @@ class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.Unicode;
 
-        var nums = new[] { 1, 7, 45, 23 };
+        List<double> list = new List<double>();
 
-        int greatest = nums.Max();//вернёт 45
-        int smallest = nums.Min();//вернёт 1
-        double average = nums.Average();//вернёт 19
-
-        Console.WriteLine($"{greatest}, {smallest}, {average}");
-
-        //Сложный тип данных
-
-        // Список студентов
-        List<Student> students = new List<Student>
+        while (true)
         {
-            new Student { Name = "Алёна" , Age =23, Languages = new List<string>{ "английский", "неменцкий" } },
-            new Student { Name = "Яков" , Age =23, Languages = new List<string>{ "английский", "неменцкий" } },
-            new Student { Name = "Андрей" , Age =23, Languages = new List<string>{ "английский", "неменцкий" } },
-            new Student { Name = "Сергей" , Age =27, Languages = new List<string>{ "английский", "французский" } },
-            new Student { Name = "Дмитрий" , Age =29, Languages = new List<string>{ "английский", "испанский" } },
-            new Student { Name = "Василий" , Age =24, Languages = new List<string>{ "испанский", "неменцкий" } },
-        };
+            Console.Clear();
 
-        //найдём возраст самого старого студента (29)
-        var oldest = students.Max(s => s.Age);
+            Console.Write("Введите число: ");
+            string str = Console.ReadLine();
 
-        //самого молодого (23)
-        var youngest = students.Min(s => s.Age);
+            if (!double.TryParse(str, out double result))
+            {
+                Console.WriteLine("Введено не верное зачение");
+                Console.ReadKey();
+                continue;
+            }
 
-        // и средний возраст (26,3)
-        var averageAge = students.Average(s => s.Age);
+            list.Add(result);
+            Console.WriteLine("Число " + result + " добавлено в список");
 
-        Console.WriteLine($"\n{oldest}\n{youngest}\n{averageAge:0.0}");
+            Console.WriteLine("\nИнформация о числовом ряде\n");
+            Console.WriteLine($"{"Количество элементов", -33} {list.Count}");
+            Console.WriteLine($"{"Сумма всех элементов", -33} {Math.Round(list.Sum(), 4)}");
+            Console.WriteLine($"{"Наибольший элемент", -33} {list.Max()}");
+            Console.WriteLine($"{"Наименьший элемент", -33} {list.Min()}");
+            Console.WriteLine($"{"Среднее занчение всех элементов", -33} {Math.Round(list.Average(), 4)}");
 
-        //средний возраст студентов, владующих английским языком
-        var avrAgeEngLang = students
-            .Where(s => s.Languages.Contains("английский"))
-            .Average(s => s.Age);
-
-        Console.WriteLine(Environment.NewLine + avrAgeEngLang);
-    }
-
-    public class Student
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public List<string> Languages { get; set; }
-
-        public override string ToString()
-        {
-            return Name + " - " + Age;
+            Console.ReadKey();
         }
-    }
+    }    
 }
